ad947bbf569fdf49a2de755ae9bed9d7ae2ac59a
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-10-30 11:07:30 +0100

    Use bcrypt

--- greenweb/settings/common.py
+++ greenweb/settings/common.py
@@ -99,6 +99,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
