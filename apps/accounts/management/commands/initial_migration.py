755842f12e2d8c40d31c7a34a6abb75e9cba3007
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-10-30 12:03:20 +0100

    Command to run the initial migration

---
+++ apps/accounts/management/commands/initial_migration.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = "Initial migration."
+
+    def handle(self, *args, **options):
+        call_command('migrate', '--fake-initial', 'accounts' '0001_initial')
+        call_command('migrate', 'accounts')
+        call_command('migrate', 'admin')
+        call_command('migrate', 'contenttypes')
+        call_command('migrate', 'auth')
+        call_command('migrate', '--fake-initial', 'greencheck', '0001_initial')
+        call_command('migrate', 'sessions')
+        self.stdout.write(self.style.SUCCESS('Initial migration completed!'))
