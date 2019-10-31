b73df79d70832b7cdd405369dffb581142503786
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-10-31 10:41:56 +0100

    Add user admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -4,10 +4,31 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
-# Register your models here.
-# @admin.register(User)
+
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ['email', 'username', 'is_staff']
+    search_fields = ('username', 'email')
+    list_display = [
+        'username',
+        'email',
+        'last_login',
+        'is_staff'
+    ]
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email',)}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
