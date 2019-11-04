e3d56a473d68fe0a30fff16ffeec612bc926e20f
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-11-04 15:56:38 +0100

    Make sure that we have password reset view, now visible on login

--- greenweb/urls.py
+++ greenweb/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.contrib import admin
 from django.urls import path, include
 
+from apps.accounts.admin_site import greenweb_admin as admin
+from django.contrib.auth import views as auth_views
 urlpatterns = []
 
 if settings.DEBUG:
@@ -26,5 +27,12 @@ if settings.DEBUG:
     ]
 
 urlpatterns += [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.urls),
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
