43547259748e37a9b0cb4848c5989e17cbd4fc8a
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-11-14 12:36:59 +0100

    Look over migrations one last time - There were a couple of missing migrations so added these into faked migrations - When migrating everything, order is important, first start with sql_migrations, then with initial_migrations

--- apps/accounts/management/commands/initial_migration.py
+++ apps/accounts/management/commands/initial_migration.py
@@ -6,11 +6,11 @@ class Command(BaseCommand):
     help = "Initial migration."
 
     def handle(self, *args, **options):
+        call_command('migrate', 'contenttypes')
+        call_command('migrate', 'auth')
         call_command('migrate', '--fake-initial', 'accounts', '0001_initial')
         call_command('migrate', 'accounts')
         call_command('migrate', 'admin')
-        call_command('migrate', 'contenttypes')
-        call_command('migrate', 'auth')
         call_command('migrate', '--fake', 'greencheck', '0001_initial')
         call_command('migrate', 'sessions')
         call_command('migrate')
--- apps/accounts/migrations/0001_initial.py
+++ apps/accounts/migrations/0001_initial.py
@@ -247,4 +247,14 @@ class Migration(migrations.Migration):
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
+        migrations.AddField(
+            model_name='user',
+            name='groups',
+            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups'),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='user_permissions',
+            field=models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions'),
+        )
     ]
