cd744e7d104c697f606db0f48cca185e1a1d7090
Chris Adams <chris@productscience.co.uk>
2019-11-18 10:07:46 +0100

    Add the allowed hosts for the admin

--- greenweb/settings/production.py
+++ greenweb/settings/production.py
@@ -5,3 +5,5 @@ ANYMAIL = {
     'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
 }
 EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+
+ALLOWED_HOSTS = ['thegreenwebfoundation.org', 'newadmin.thegreenwebfoundation.org']
