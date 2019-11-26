45d9fc48cb683c85560987e308b87d5689943eba
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-11-26 19:37:15 +0100

    Make sure that things are actually logged in production

--- greenweb/settings/common.py
+++ greenweb/settings/common.py
@@ -143,3 +143,36 @@ STATIC_URL = '/static/'
 # Media Files
 MEDIA_ROOT = root('media')
 MEDIA_URL = '/media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG'
+    },
+    'handlers': {
+        'console': {
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            # uncomment to see all queries
+            # 'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
--- greenweb/settings/development.py
+++ greenweb/settings/development.py
@@ -15,35 +15,3 @@ INSTALLED_APPS.insert(0, 'whitenoise.runserver_nostatic')
 MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG'
-    },
-    'handlers': {
-        'console': {
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-    },
-    'formatters': {
-        'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'loggers': {
-        'django.db.backends': {
-            # uncomment to see all queries
-            # 'level': 'DEBUG',
-            'handlers': ['console'],
-        }
-    }
-}
