70365a5ff87ed74778616e88607f0d6c1918761c
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-12-09 20:32:04 +0100

    Uploading to gcp is optional, add a flag to activate upload

--- apps/greencheck/management/commands/dump_greenpresenting.py
+++ apps/greencheck/management/commands/dump_greenpresenting.py
@@ -12,6 +12,9 @@ from google.cloud import storage
 class Command(BaseCommand):
     help = "Dump green_presenting table into sqlite."
 
+    def add_arguments(self, parser):
+        parser.add_argument('--upload', help='Also upload to GCP')
+
     def handle(self, *args, **options):
         root = Path(settings.ROOT)
         database_url = os.environ.get('DATABASE_URL')
@@ -30,9 +33,11 @@ class Command(BaseCommand):
         presenting_table.create_index(['url'])
         presenting_table.create_index(['hosted_by'])
 
-        client = storage.Client()
-        bucket_name = settings.PRESENTING_BUCKET
-        bucket = client.get_bucket(bucket_name)
-        blob = bucket.blob(db_name)
-        blob.upload_from_filename(str(root / db_name))
+        upload = options['upload']
+        if upload:
+            client = storage.Client()
+            bucket_name = settings.PRESENTING_BUCKET
+            bucket = client.get_bucket(bucket_name)
+            blob = bucket.blob(db_name)
+            blob.upload_from_filename(str(root / db_name))
 
