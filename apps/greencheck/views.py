a5a915703888c806c34f70db0054d167a21064a6
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-12-09 16:20:53 +0100

    Add the view to show where you can download dump

--- apps/greencheck/views.py
+++ apps/greencheck/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from datetime import date
+from datetime import timedelta
 
-# Create your views here.
+from django.conf import settings
+from google.cloud import storage
+from django.views.generic.base import TemplateView
+
+
+class GreenUrlsView(TemplateView):
+    template_name = "green_url.html"
+
+    def fetch_urls(self):
+        client = storage.Client()
+        bucket_name = settings.PRESENTING_BUCKET
+        bucket = client.get_bucket(bucket_name)
+        blobs = bucket.list_blobs()
+        return [(b.name, b.public_url) for b in blobs]
+
+    @property
+    def urls(self):
+        '''
+        Setting the date two weeks in the future. Two weeks from now on
+        it will prefetch the urls again
+        '''
+        accessed_date = getattr(self, '_date', None)
+        if not accessed_date or self._date < date.today():
+            self._date = date.today() + timedelta(weeks=2)
+            self._urls = self.fetch_urls()
+
+        return self._urls
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['urls'] = self.urls
+        return context
+
+
