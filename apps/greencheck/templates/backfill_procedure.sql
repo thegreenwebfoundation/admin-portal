061644497c19b28531fc9080b4e1f13e461320af
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-12-09 11:04:22 +0100

    Parametrise backfill with date This allows to just select a subset of the data to backfill

--- apps/greencheck/templates/backfill_procedure.sql
+++ apps/greencheck/templates/backfill_procedure.sql
@@ -1,6 +1,6 @@
 DROP PROCEDURE IF EXISTS backfill;
 
-CREATE PROCEDURE backfill()
+CREATE PROCEDURE backfill(IN date_begin Date)
 BEGIN
   DECLARE done INT DEFAULT FALSE;
 
@@ -8,7 +8,7 @@ BEGIN
   DECLARE ggreen TINYINT(2);
   DECLARE gid_hp INT(11);
   DECLARE gurl VARCHAR(255);
-  DECLARE cur CURSOR FOR SELECT datum, green, id_hp, url FROM greencheck where id_hp > 0;
+  DECLARE cur CURSOR FOR SELECT datum, green, id_hp, url FROM greencheck where id_hp > 0 AND datum > date_begin;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
   OPEN cur;
