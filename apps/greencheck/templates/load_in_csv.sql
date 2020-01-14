c5d89f286e28569ecd2d6f2a2daa9757b92ced20
Chris Adams <chris@productscience.co.uk>
2020-01-14 15:48:48 +0100

    Add simple statement for temporary url table

--- apps/greencheck/templates/load_in_csv.sql
+++ apps/greencheck/templates/load_in_csv.sql
@@ -1,13 +1,14 @@
+-- a temporary table to facilitate the creation of the green urls table
 CREATE TABLE urls (
   url VARCHAR(255),
   PRIMARY KEY (url)
 );
 
-LOAD DATA
-  INFILE 'all_domains_outfile.txt'
-INTO TABLE
-  urls
-FIELDS TERMINATED BY ','
-ENCLOSED BY ''
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- LOAD DATA
+--   INFILE 'all_domains_outfile.txt'
+-- INTO TABLE
+--   urls
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY ''
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
