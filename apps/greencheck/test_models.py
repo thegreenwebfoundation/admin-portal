c20f4a920b08f7921ce52b6ed9fbdc3ee12f83ed
Chris Adams <chris@productscience.co.uk>
2020-03-21 13:45:14 +0100

    We don't need to look for existing green Ips

--- apps/greencheck/test_models.py
+++ apps/greencheck/test_models.py
@@ -8,8 +8,8 @@ from apps.greencheck.models import GreencheckIp
 class TestGreenCheckIP:
 
     def test_greencheckip_has_start_and_end(self, hosting_provider, db):
-
-        gcip = GreencheckIp.objects.update_or_create(
+        hosting_provider.save()
+        gcip = GreencheckIp.objects.create(
             active=True,
             ip_start="127.0.0.1",
             ip_end="120.0.0.1",
