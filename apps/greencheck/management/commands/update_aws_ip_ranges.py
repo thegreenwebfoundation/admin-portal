41dc68c9fd9e41869e12b6b43016e4c5986af8ba
Chris Adams <chris@productscience.co.uk>
2020-03-21 17:05:59 +0100

    formt with black

--- apps/greencheck/management/commands/update_aws_ip_ranges.py
+++ apps/greencheck/management/commands/update_aws_ip_ranges.py
@@ -11,17 +11,18 @@ from django.db import connection
 logger = logging.getLogger(__name__)
 
 GREEN_REGIONS = (
-    ('Amazon US West', 'us-west-2', 696),
-    ('Amazon EU (Frankfurt)', 'eu-central-1', 697),
-    ('Amazon EU (Ireland)', 'eu-west-1', 698),
-    ('Amazon AWS GovCloud (USA)','us-gov-west-1', 699),
-    ('Amazon Montreal', 'ca-central-1', 700),
+    ("Amazon US West", "us-west-2", 696),
+    ("Amazon EU (Frankfurt)", "eu-central-1", 697),
+    ("Amazon EU (Ireland)", "eu-west-1", 698),
+    ("Amazon AWS GovCloud (USA)", "us-gov-west-1", 699),
+    ("Amazon Montreal", "ca-central-1", 700),
 )
-class AmazonCloudProvider:
 
+
+class AmazonCloudProvider:
     def __init__(self, *args, **kwargs):
-        if kwargs.get('green_regions'):
-            self.green_regions = kwargs.get('green_regions')
+        if kwargs.get("green_regions"):
+            self.green_regions = kwargs.get("green_regions")
         else:
             self.green_regions = GREEN_REGIONS
 
@@ -36,7 +37,9 @@ class AmazonCloudProvider:
 
             # pull out the ip ranges as strings
             green_ipv4s = self.pullout_green_regions(ip_ranges, aws_code)
-            green_ipv6s = self.pullout_green_regions(ip_ranges, aws_code, ip_version="ipv6")
+            green_ipv6s = self.pullout_green_regions(
+                ip_ranges, aws_code, ip_version="ipv6"
+            )
 
             try:
                 logger.info(f"Looking IPs for {region_name}")
@@ -45,21 +48,26 @@ class AmazonCloudProvider:
                 logger.warning(f"Hoster {region_name} not found")
                 continue
 
-
             # then convert them to ip networks
             green_ipv4_ranges = self.ip_ranges_for_hoster(green_ipv4s)
-            green_ipv6_ranges = self.ip_ranges_for_hoster(green_ipv6s, ip_version="ipv6")
+            green_ipv6_ranges = self.ip_ranges_for_hoster(
+                green_ipv6s, ip_version="ipv6"
+            )
 
-            logger.info(f"Found {len(green_ipv4_ranges)} ipv4 and {len(green_ipv6_ranges)} ipv6 network ranges for {region_name}. Adding new ones to database")
+            logger.info(
+                f"Found {len(green_ipv4_ranges)} ipv4 and {len(green_ipv6_ranges)} ipv6 network ranges for {region_name}. Adding new ones to database"
+            )
 
-            assert(len(green_ipv4_ranges) > 0)
-            assert(len(green_ipv6_ranges) > 0)
+            assert len(green_ipv4_ranges) > 0
+            assert len(green_ipv6_ranges) > 0
 
             # we need to do this for ipv4, and then ipv6
-            res.append({
-            'ipv4' : self.add_ip_ranges_to_hoster(hoster, green_ipv4_ranges),
-            'ipv6' : self.add_ip_ranges_to_hoster(hoster, green_ipv6_ranges),
-            })
+            res.append(
+                {
+                    "ipv4": self.add_ip_ranges_to_hoster(hoster, green_ipv4_ranges),
+                    "ipv6": self.add_ip_ranges_to_hoster(hoster, green_ipv6_ranges),
+                }
+            )
 
         return res
 
@@ -73,17 +81,17 @@ class AmazonCloudProvider:
         """
 
         if ip_version == "ipv6":
-            prefix = 'ipv6_prefixes'
+            prefix = "ipv6_prefixes"
             aws_lookup_key = "ipv6_prefix"
         else:
             prefix = "prefixes"
             aws_lookup_key = "ip_prefix"
 
         return [
-            aws_ip[aws_lookup_key] for aws_ip in ip_ranges[prefix]
-            if aws_ip['region'] == region
+            aws_ip[aws_lookup_key]
+            for aws_ip in ip_ranges[prefix]
+            if aws_ip["region"] == region
         ]
-
 
     def ip_ranges_for_hoster(self, ip_ranges, ip_version="ipv4"):
         if ip_version == "ipv6":
@@ -111,19 +119,22 @@ class AmazonCloudProvider:
 
         return results
 
-    def update_hoster(self, hoster: Hostingprovider, first: ipaddress.IPv4Address, last: ipaddress.IPv4Address):
+    def update_hoster(
+        self,
+        hoster: Hostingprovider,
+        first: ipaddress.IPv4Address,
+        last: ipaddress.IPv4Address,
+    ):
         # use the ORM to update the deets for the corresponding hoster
         gcip, created = GreencheckIp.objects.update_or_create(
-            active=True,
-            ip_start=first,
-            ip_end=last,
-            hostingprovider=hoster
+            active=True, ip_start=first, ip_end=last, hostingprovider=hoster
         )
         gcip.save()
 
         if created:
             logger.debug(gcip)
             return gcip
+
 
 class Command(BaseCommand):
     help = "Update IP ranges for cloud providers that publish them"
@@ -134,11 +145,8 @@ class Command(BaseCommand):
         logger.info("Adding ranges")
         update_result = aws.update_ranges(ip_ranges)
         for region in update_result:
-            green_ipv4s, green_ipv6s = region.get('ipv4'), region.get('ipv6')
+            green_ipv4s, green_ipv6s = region.get("ipv4"), region.get("ipv6")
             self.stdout.write(
                 f"Import Complete: Added {len(green_ipv4s)} new IPV4 addresses, "
                 f"and {len(green_ipv6s) } IPV6 addresses"
             )
-
-
-
