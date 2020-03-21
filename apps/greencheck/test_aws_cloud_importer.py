84e2f438104b46a07559306c5379c325eed410dd
Chris Adams <chris@productscience.co.uk>
2020-03-21 17:05:59 +0100

    formt with black

--- apps/greencheck/test_aws_cloud_importer.py
+++ apps/greencheck/test_aws_cloud_importer.py
@@ -14,36 +14,35 @@ from apps.greencheck.management.commands.update_aws_ip_ranges import GREEN_REGIO
 @pytest.fixture
 def hosting_provider():
 
-    oregon, *rest = [
-        region for region in GREEN_REGIONS
-        if region[1] == "us-west-2"
-    ]
+    oregon, *rest = [region for region in GREEN_REGIONS if region[1] == "us-west-2"]
     name, region, host_id = oregon
     return Hostingprovider(
-        archived= False,
-        country= 'US',
-        customer= False,
-        icon= '',
-        iconurl= '',
-        id = host_id,
-        model= 'groeneenergie',
-        name= name,
-        partner= '',
-        showonwebsite= True,
-        website= 'http://aws.amazon.com'
+        archived=False,
+        country="US",
+        customer=False,
+        icon="",
+        iconurl="",
+        id=host_id,
+        model="groeneenergie",
+        name=name,
+        partner="",
+        showonwebsite=True,
+        website="http://aws.amazon.com",
     )
+
 
 @pytest.fixture
 def aws_cloud_provider(hosting_provider):
     hosting_provider.save()
-    return update_aws_ip_ranges.AmazonCloudProvider(green_regions=(
-        ('Amazon US West', 'us-west-2', hosting_provider.id),
-    ))
+    return update_aws_ip_ranges.AmazonCloudProvider(
+        green_regions=(("Amazon US West", "us-west-2", hosting_provider.id),)
+    )
+
 
 @pytest.fixture
 def aws_json_ip_ranges():
     this_file = pathlib.Path(__file__)
-    json_path = this_file.parent.joinpath('fixtures', "ip_ranges.json")
+    json_path = this_file.parent.joinpath("fixtures", "ip_ranges.json")
     with open(json_path) as ipr:
         ip_ranges = json.loads(ipr.read())
         return ip_ranges
@@ -51,7 +50,6 @@ def aws_json_ip_ranges():
 
 @pytest.mark.django_db
 class TestAWSCLoudImporter:
-
     def test_fetch_ip(self, hosting_provider, aws_cloud_provider):
         """
         Do we fetch the data from AWS?
@@ -59,7 +57,7 @@ class TestAWSCLoudImporter:
         hosting_provider.save()
         res = aws_cloud_provider.fetch_ip_ranges()
 
-        assert(len(res.keys()) == 4 )
+        assert len(res.keys()) == 4
 
     def pullout_green_regions(self, hosting_provider, aws_cloud_provider):
         res = aws_cloud_provider.fetch_ip_ranges()
@@ -70,7 +68,7 @@ class TestAWSCLoudImporter:
 
         ip_ranges = aws_cloud_provider.ip_ranges_for_hoster(iprs)
 
-        assert(isinstance(ip_ranges[0], ipaddress.IPv4Network))
+        assert isinstance(ip_ranges[0], ipaddress.IPv4Network)
 
     def test_update_hoster(self, hosting_provider, aws_cloud_provider):
 
@@ -80,28 +78,28 @@ class TestAWSCLoudImporter:
         ip_ranges = aws_cloud_provider.ip_ranges_for_hoster(iprs)
 
         ip_start, ip_end = ip_ranges[0][0], ip_ranges[0][-1]
-        assert(GreencheckIp.objects.all().count() == 0)
+        assert GreencheckIp.objects.all().count() == 0
 
         hosting_provider.save()
 
         # create one new green IP range
         aws_cloud_provider.update_hoster(hosting_provider, ip_start, ip_end)
 
-        assert(GreencheckIp.objects.all().count() == 1)
+        assert GreencheckIp.objects.all().count() == 1
 
-    def test_update_range(self, hosting_provider, aws_cloud_provider, aws_json_ip_ranges):
-        assert(GreencheckIp.objects.all().count() == 0)
+    def test_update_range(
+        self, hosting_provider, aws_cloud_provider, aws_json_ip_ranges
+    ):
+        assert GreencheckIp.objects.all().count() == 0
         hosting_provider.save()
 
         res, *rest = aws_cloud_provider.update_ranges(aws_json_ip_ranges)
-        ipv4s = res['ipv4']
-        ipv6s = res['ipv6']
-
-
-        assert(len(ipv4s) == 104)
-        assert(len(ipv6s) == 20)
+        ipv4s = res["ipv4"]
+        ipv6s = res["ipv6"]
+        assert len(ipv4s) == 104
+        assert len(ipv6s) == 20
         # we should have 124 ranges in total
-        assert(GreencheckIp.objects.all().count() == len(ipv4s) + len(ipv6s))
+        assert GreencheckIp.objects.all().count() == len(ipv4s) + len(ipv6s)
 
 
 @pytest.mark.django_db
@@ -113,6 +111,5 @@ class TestAWSImportCommand:
 
     def test_handle(self, aws_cloud_provider):
         out = StringIO()
-        call_command('update_aws_ip_ranges', stdout=out)
-        assert('Import Complete:' in out.getvalue())
-
+        call_command("update_aws_ip_ranges", stdout=out)
+        assert "Import Complete:" in out.getvalue()
