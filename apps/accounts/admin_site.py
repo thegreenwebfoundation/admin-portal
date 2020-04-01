b9a9f6042d6c68e910673a928a56d9df4f525bf0
Chris Adams <chris@productscience.co.uk>
2020-04-01 19:33:13 +0200

    Reformat code with black and isort

--- apps/accounts/admin_site.py
+++ apps/accounts/admin_site.py
@@ -1,37 +1,31 @@
+import json
 import re
-from urllib.parse import urlparse
 import socket
-from ipwhois import IPWhois
+from urllib.parse import urlparse
 
+import requests
+from django import forms
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ValidationError
-from django.urls import path
-from django.urls import reverse
-from django.shortcuts import render
 from django.core.validators import URLValidator
-
-
-import json
-
-import requests
-from requests.exceptions import HTTPError
-
+from django.shortcuts import render
+from django.urls import path, reverse
 from django.views.generic.edit import FormView
-from django import forms
+from ipwhois import IPWhois
+from requests.exceptions import HTTPError
 
 from apps.greencheck.views import GreenUrlsView
 
-
-URL_RE = re.compile(r'https?:\/\/(.*)')
-BASE_URL = 'https://api.thegreenwebfoundation.org/greencheck'
+URL_RE = re.compile(r"https?:\/\/(.*)")
+BASE_URL = "https://api.thegreenwebfoundation.org/greencheck"
 
 
 class CheckUrlForm(forms.Form):
     url = forms.URLField()
 
     def clean_url(self):
-        url = self.cleaned_data['url']
+        url = self.cleaned_data["url"]
         url_check = URLValidator()
         # if this is valid, nothing should be raised
         url_check(url)
@@ -39,39 +33,37 @@ class CheckUrlForm(forms.Form):
 
 
 class CheckUrlView(FormView):
-    template_name = 'try_out.html'
+    template_name = "try_out.html"
     form_class = CheckUrlForm
-    success_url = '/not/used'
+    success_url = "/not/used"
 
     def greencheck(self, domain):
-        resp = requests.get(f'{BASE_URL}/{domain}')
+        resp = requests.get(f"{BASE_URL}/{domain}")
         resp.raise_for_status()
         return resp.json()
 
     def whois_check(self, domain):
         ip_address = socket.gethostbyname(domain)
-
         obj = IPWhois(ip_address)
         return obj.lookup_rdap(depth=1)
-        # import ipdb ; ipdb.set_trace()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form_url'] = reverse('admin:check_url')
+        context["form_url"] = reverse("admin:check_url")
         return context
 
     def form_valid(self, form):
-        url = form.data.get('url')
+        url = form.data.get("url")
         domain = urlparse(url).netloc
 
         greencheck = self.greencheck(domain)
         whois_check = self.whois_check(domain)
-        green = greencheck.get('green')
+        green = greencheck.get("green")
         context = self.get_context_data()
 
-        context['green_status'] = 'green' if green else 'gray'
-        context['whois_check'] = whois_check
-        context['greencheck'] = greencheck
+        context["green_status"] = "green" if green else "gray"
+        context["whois_check"] = whois_check
+        context["greencheck"] = greencheck
 
         return render(self.request, self.template_name, context)
 
@@ -79,11 +71,11 @@ class CheckUrlView(FormView):
 class GreenWebAdmin(AdminSite):
     # This is a standard authentication form that allows non-staff users
     login_form = AuthenticationForm
-    index_template = 'admin_index.html'
-    site_header = 'The Green Web Foundation Administration Site'
-    index_title = 'The Green Web Foundation Administration Site'
-    login_template = 'login.html'
-    logout_template = 'logout.html'
+    index_template = "admin_index.html"
+    site_header = "The Green Web Foundation Administration Site"
+    index_title = "The Green Web Foundation Administration Site"
+    login_template = "login.html"
+    logout_template = "logout.html"
 
     def has_permission(self, request):
         """
@@ -95,8 +87,8 @@ class GreenWebAdmin(AdminSite):
     def get_urls(self):
         urls = super().get_urls()
         patterns = [
-            path('try_out/', CheckUrlView.as_view(), name='check_url'),
-            path('green-urls', GreenUrlsView.as_view(), name='green_urls'),
+            path("try_out/", CheckUrlView.as_view(), name="check_url"),
+            path("green-urls", GreenUrlsView.as_view(), name="green_urls"),
         ]
         return patterns + urls
 
@@ -106,12 +98,12 @@ class GreenWebAdmin(AdminSite):
             {
                 "name": "Try out greencheck",
                 "app_label": "greencheck",
-                "app_url": reverse('admin:check_url'),
+                "app_url": reverse("admin:check_url"),
                 "models": [
                     {
                         "name": "Try out a url",
                         "object_name": "greencheck_url",
-                        "admin_url": reverse('admin:check_url'),
+                        "admin_url": reverse("admin:check_url"),
                         "view_only": True,
                     }
                 ],
@@ -119,18 +111,18 @@ class GreenWebAdmin(AdminSite):
             {
                 "name": "Download data dump",
                 "app_label": "greencheck",
-                "app_url": reverse('admin:check_url'),
+                "app_url": reverse("admin:check_url"),
                 "models": [
                     {
                         "name": "Download data dump",
                         "object_name": "greencheck_url",
-                        "admin_url": reverse('admin:green_urls'),
+                        "admin_url": reverse("admin:green_urls"),
                         "view_only": True,
                     }
                 ],
-            }
+            },
         ]
         return app_list
 
 
-greenweb_admin = GreenWebAdmin(name='greenweb_admin')
+greenweb_admin = GreenWebAdmin(name="greenweb_admin")
