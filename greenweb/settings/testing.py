2208edd4222af5f21e63362e8df1583f9703f447
Chris Adams <chris@productscience.co.uk>
2020-04-01 19:31:23 +0200

    Change testing setup to account for whitenoise

--- greenweb/settings/testing.py
+++ greenweb/settings/testing.py
@@ -1,5 +1,19 @@
 import socket
 from .common import * # noqa
 
+import logging
+
+from whitenoise.storage import CompressedManifestStaticFilesStorage
+
+logger = logging.getLogger(__name__)
+
 INTERNAL_IPS = ['127.0.0.1']
 ALLOWED_HOSTS.extend(['127.0.0.1', 'localhost'])
+
+
+# In testing, using whitenoise here doesn't really help us, so we
+# switch back to the default instead.
+# This is recommended by Django maintainers, and referred to in
+# the whitenoise docs
+# https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#django.contrib.staticfiles.storage.ManifestStaticFilesStorage.manifest_strict
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
