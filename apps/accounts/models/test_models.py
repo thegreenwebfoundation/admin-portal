1f3a9c409b202ef7640a67b59bc6ab305824ddd4
Chris Adams <chris@productscience.co.uk>
2020-06-03 14:07:04 +0200

    Remove stray ipdb, add note about slow tests

--- apps/accounts/models/test_models.py
+++ apps/accounts/models/test_models.py
@@ -3,6 +3,8 @@ import pytest
 from apps.accounts.models import Datacenter
 from apps.accounts.models.choices import ModelType
 
+# TODO these hit the database, when they probably don't need to, and this will make tests slow. If we can test that these objects are valid another way we should - maybe checking at the field level, or form level.
+
 class TestDatacenter:
 
     @pytest.mark.parametrize("accounting_model", ("groeneenergie","mixed","compensatie",))
@@ -24,8 +26,6 @@ class TestHostingProvider:
     @pytest.mark.parametrize("accounting_model", ("groeneenergie","mixed","compensatie",))
     def test_accepted_ways_to_model(self, datacenter, db, sample_hoster_user, accounting_model, hosting_provider):
 
-
-        # import ipdb ; ipdb.set_trace()
         hosting_provider.save()
         sample_hoster_user.hostingprovider = hosting_provider
         sample_hoster_user.save()
