38c5e878e270f9a7bcb78d4df01ec3761cd41fb6
Chris Adams <chris@productscience.co.uk>
2020-06-05 12:33:40 +0200

    Add a template to link to the user able to update a given hosting provider

--- greenweb/settings/common.py
+++ greenweb/settings/common.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'greenweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
