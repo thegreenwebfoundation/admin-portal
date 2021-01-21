880ba9bf2a524209da01a2a7971e827bbf4088d4
Chris Adams <chris@productscience.co.uk>
2021-01-21 08:30:15 +0100

    Add better error handling for various connection issues

--- apps/greencheck/management/commands/consume_logger_queue.py
+++ apps/greencheck/management/commands/consume_logger_queue.py
@@ -17,6 +17,9 @@ class Command(BaseCommand):
 
         sitecheck_logger = LegacySiteCheckLogger()
 
+
+
+
         def on_message(channel, method_frame, header_frame, body):
             logger.debug(f"message received for {channel}")
             logger.debug(f"method_frame: {method_frame}")
@@ -31,17 +34,34 @@ class Command(BaseCommand):
         mq_connection = pika.BlockingConnection(parameters)
         channel = mq_connection.channel()
 
-        queue_args = {'x-max-priority': 4}
-        channel.queue_declare('enqueue.app.default', durable=True, arguments=queue_args)
-        channel.basic_consume('enqueue.app.default', on_message, auto_ack=True)
+        while True:
 
-        try:
-            channel.start_consuming()
-        except KeyboardInterrupt:
-            channel.stop_consuming()
-        except Exception as err:
-            logger.exception(err)
-            channel.stop_consuming()
+            try:
+                queue_args = {'x-max-priority': 4}
+                channel.queue_declare(
+                    'enqueue.app.default',
+                    durable=True,
+                    arguments=queue_args
+                )
+                channel.basic_consume('enqueue.app.default', on_message, auto_ack=True)
+                channel.start_consuming()
+
+            # Don't recover if connection was closed by broker
+            except pika.exceptions.ConnectionClosedByBroker:
+                break
+            # Don't recover on channel errors
+            except pika.exceptions.AMQPChannelError:
+                break
+            # Recover on all other connection errors
+            except pika.exceptions.AMQPConnectionError:
+                continue
+            except KeyboardInterrupt:
+                channel.stop_consuming()
+                break
+            except Exception as err:
+                logger.exception(err)
+                channel.stop_consuming()
+
 
         mq_connection.close()
 
