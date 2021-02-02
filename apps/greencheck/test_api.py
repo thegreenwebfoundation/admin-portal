2c2b86aa60062bdcc86ed1a141d70e1d0aca5519
Chris Adams <chris@productscience.co.uk>
2021-02-02 14:34:47 +0100

    Add first test for creating IP Ranges via the API

--- apps/greencheck/test_api.py
+++ apps/greencheck/test_api.py
@@ -25,7 +25,6 @@ logger = logging.getLogger(__name__)
 rf = APIRequestFactory()
 
 
-@pytest.mark.only
 class TestIpRangeViewSetList:
     def test_get_ip_ranges_empty(
         self, hosting_provider: Hostingprovider, sample_hoster_user: User,
@@ -127,7 +126,87 @@ class TestIpRangeViewSetList:
         assert len(response.data) == 0
 
 
-@pytest.mark.only
 class TestIpRangeViewSetRetrieve:
     """
+    Can we fetch a specific IP Range object to inspect?
     """
+
+    def test_get_ip_range_for_hostingprovider_by_id(
+        self,
+        hosting_provider: Hostingprovider,
+        sample_hoster_user: User,
+        green_ip: GreencheckIp,
+    ):
+
+        hosting_provider.save()
+        sample_hoster_user.hostingprovider = hosting_provider
+        sample_hoster_user.save()
+
+        rf = APIRequestFactory()
+        url_path = reverse("ip-range-detail", kwargs={"pk": green_ip.id})
+        request = rf.get(url_path)
+        request.user = sample_hoster_user
+
+        # GET end point for IP Ranges
+        view = IPRangeViewSet.as_view({"get": "retrieve"})
+
+        response = view(request, pk=green_ip.id)
+
+        assert response.status_code == 200
+
+        assert response.data["ip_start"] == green_ip.ip_start
+        assert response.data["ip_end"] == green_ip.ip_end
+        assert response.data["hostingprovider"] == green_ip.hostingprovider.id
+        assert response.data["active"] == green_ip.active
+
+
+@pytest.mark.only
+class TestIpRangeViewSetCreate:
+    def test_create_new_ip_range(
+        self,
+        hosting_provider: Hostingprovider,
+        sample_hoster_user: User,
+        green_ip: GreencheckIp,
+    ):
+
+        hosting_provider.save()
+        sample_hoster_user.hostingprovider = hosting_provider
+        sample_hoster_user.save()
+        GreencheckIp.objects.count() == 1
+
+        rf = APIRequestFactory()
+        url_path = reverse("ip-range-list")
+
+        sample_json = {
+            "hostingprovider": hosting_provider.id,
+            "ip_start": "192.168.178.121",
+            "ip_end": "192.168.178.129",
+        }
+
+        request = rf.post(url_path, sample_json)
+        request.user = sample_hoster_user
+
+        view = IPRangeViewSet.as_view({"post": "create"})
+
+        response = view(request)
+
+        assert response.status_code == 201
+        GreencheckIp.objects.count() == 2
+
+        assert response.data["ip_start"] == "192.168.178.121"
+        assert response.data["ip_end"] == "192.168.178.129"
+        assert response.data["hostingprovider"] == hosting_provider.id
+
+    @pytest.mark.skip(reason="Pending. ")
+    def test_skip_duplicate_ip_range(
+        self,
+        hosting_provider: Hostingprovider,
+        sample_hoster_user: User,
+        green_ip: GreencheckIp,
+    ):
+        """
+        When a user creates an IP Range, we want to avoid the case of them
+        making a duplicate. IP Range. Even if we check in the serialiser
+        class, we should make sure a sensible API error message is returned
+        """
+        pass
