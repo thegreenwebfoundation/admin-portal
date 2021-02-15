13d116a3fa382c146ba01c128c396707e0033975
Chris Adams <chris@productscience.co.uk>
2021-02-15 11:57:40 +0100

    Add missing import

--- apps/greencheck/viewsets.py
+++ apps/greencheck/viewsets.py
@@ -16,6 +16,7 @@ from rest_framework.authentication import BasicAuthentication, SessionAuthentica
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.settings import api_settings
+from rest_framework.decorators import api_view
 from rest_framework import mixins
 from rest_framework_csv import renderers as drf_csv_rndr
 
