d8b586bf21eef68410e1a2134ec34a120e16b6b9
Chris Adams <chris@productscience.co.uk>
2021-05-22 15:22:53 +0200

    Swap in less strict static storage for tests

--- greenweb/settings/testing.py
+++ greenweb/settings/testing.py
@@ -4,6 +4,8 @@ INTERNAL_IPS = ["127.0.0.1"]
 ALLOWED_HOSTS.extend(["127.0.0.1", "localhost"])  # noqa
 DOMAIN_SNAPSHOT_BUCKET = "tgwf-green-domains-test"
 
+# http://whitenoise.evans.io/en/stable/django.html#WHITENOISE_MANIFEST_STRICT
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # override settings, so we don't need to run rabbitmq in testing
 # For more, see https://github.com/Bogdanp/django_dramatiq#testing
