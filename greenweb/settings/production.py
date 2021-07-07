8dd92f36c24ce9a46c4b68e80407ffeb8a2ea530
Chris Adams <chris@productscience.co.uk>
2021-07-07 18:23:19 +0200

    Add S3 compatible object storage for attachments

--- greenweb/settings/production.py
+++ greenweb/settings/production.py
@@ -21,6 +21,16 @@ ALLOWED_HOSTS = [
 
 
 DOMAIN_SNAPSHOT_BUCKET = "tgwf-green-domains-live"
+# Used by django storages for storing files
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# Scaleway deets
+AWS_ACCESS_KEY_ID = env("OBJECT_STORAGE_ACCESS_KEY_ID")  # noqa
+AWS_SECRET_ACCESS_KEY = env("OBJECT_STORAGE_SECRET_ACCESS_KEY")  # noqa
+AWS_STORAGE_BUCKET_NAME = env("OBJECT_STORAGE_BUCKET_NAME")  # noqa
+AWS_S3_REGION_NAME = env("OBJECT_STORAGE_REGION")  # noqa
+AWS_S3_ENDPOINT_URL = env("OBJECT_STORAGE_ENDPOINT")  # noqa
+
 
 # report when things asplode
 sentry_dsn = os.environ.get("SENTRY_DSN", False)  # noqa
