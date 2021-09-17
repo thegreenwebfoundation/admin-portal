ccd3c52d49e82b9f28c44a2a5ddb38bb7fb3c9ce
Chris Adams <chris@productscience.co.uk>
2021-09-17 11:59:37 +0200

    Update our domain check code to support as checking via carbon.txt

--- apps/greencheck/tests/management/test_import_carbon_txt.py
+++ apps/greencheck/tests/management/test_import_carbon_txt.py
@@ -2,8 +2,15 @@ import logging
 import pytest
 import io
 from django.core import management
+from django.urls import reverse
 
+import dramatiq
+import rich
+from dramatiq.brokers import stub
 from ....accounts import models as ac_models
+from ... import models as gc_models
+from ... import workers
+from ... import domain_check
 
 
 logger = logging.getLogger(__name__)
@@ -36,3 +43,30 @@ class TestImportFromCarbonTxt:
         for name in names:
             assert name in out.getvalue()
 
+    @pytest.mark.only
+    def test_import_from_url_then_check_against_api(self, db, client):
+        """
+        Sanity check to make sure we don't override the imported
+        information with our async lookup process.
+        """
+        out = io.StringIO()
+        # broker.declare_queue("default")
+
+        url = "bergfreunde.it"
+
+        # run an import
+        management.call_command(
+            "import_from_carbon_txt_url",
+            "https://www.bergfreunde.it/carbon.txt",
+            stdout=out,
+        )
+
+        checker = domain_check.GreenDomainChecker()
+        sitecheck = checker.check_domain(url)
+
+        # we use the sitecheck status to keep the GreenDomain
+        # table up to date, and our async workers update the status in the Greendomain table after each check.
+        # Checking the status here saves us needing to have all
+        # the async checking infra with workers, and brokers in our
+        # test
+        assert sitecheck.green
