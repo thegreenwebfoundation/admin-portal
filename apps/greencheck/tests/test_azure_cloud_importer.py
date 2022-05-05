663dc87b94be2bcdfba4c96f07eb59ab40231dab
Chris Adams <chris@productscience.co.uk>
2022-05-05 17:47:59 +0200

    Format with black, some stylistic changes

--- apps/greencheck/tests/test_azure_cloud_importer.py
+++ apps/greencheck/tests/test_azure_cloud_importer.py
@@ -1,6 +1,5 @@
 import pytest
 import pathlib
-import ipaddress
 import json
 from io import StringIO
 
@@ -16,20 +15,20 @@ from django.conf import settings
 def hosting_provider():
     """
     Define necessary information of a hosting provider for testing.
-    Return: an initialization of a Hostingprovider object 
+    Return: an initialization of a Hostingprovider object
     """
     return Hostingprovider(
-        archived = False,
-        country = "US",
-        customer = False,
-        icon = "",
-        iconurl = "",
-        id = settings.AZURE_PROVIDER_ID, # host id
-        model = "groeneenergie",
-        name = "Azure US West",
-        partner = "",
-        showonwebsite = True,
-        website = "http://azure.microsoft.com",
+        archived=False,
+        country="US",
+        customer=False,
+        icon="",
+        iconurl="",
+        id=settings.AZURE_PROVIDER_ID,  # host id
+        model="groeneenergie",
+        name="Azure US West",
+        partner="",
+        showonwebsite=True,
+        website="http://azure.microsoft.com",
     )
 
 
@@ -37,10 +36,11 @@ def hosting_provider():
 def azure_cloud_provider(hosting_provider):
     """
     Prepare the cloud provider.
-    Return: an initialization of a MicrosoftCloudProvider object from file update_azure_ip_ranges.py
+    Return: MicrosoftCloudProvider
     """
-    # Propperly initialize the hosting provider so it retrieves necessary values such as an id
-    hosting_provider.save() 
+    # Properly initialize the hosting provider so it
+    # retrieves necessary values such as an id
+    hosting_provider.save()
     return update_azure_ip_ranges.MicrosoftCloudProvider()
 
 
@@ -58,7 +58,9 @@ def azure_test_dataset():
 
 @pytest.mark.django_db
 class TestAZURECLoudImporter:
-    def test_dataset_structure(self, hosting_provider, azure_cloud_provider, azure_test_dataset):
+    def test_dataset_structure(
+        self, hosting_provider, azure_cloud_provider, azure_test_dataset
+    ):
         """
         Test if the structure of the JSON is as expected
         """
@@ -67,25 +69,28 @@ class TestAZURECLoudImporter:
         # Test: structure contains the main three keys
         assert len(azure_test_dataset.keys()) == 3
 
-        # Test: fields are available for traversing 
-        assert azure_test_dataset['values'][0]['properties']['addressPrefixes']
-        
-    def test_inserting_range(self, hosting_provider, azure_cloud_provider, azure_test_dataset):
+        # Test: fields are available for traversing
+        assert azure_test_dataset["values"][0]["properties"]["addressPrefixes"]
+
+    def test_inserting_range(
+        self, hosting_provider, azure_cloud_provider, azure_test_dataset
+    ):
         """
         Test the insertion of a new range
         """
-        _, host_id = ("Azure", settings.AZURE_PROVIDER_ID)
 
         ip_ranges = azure_cloud_provider.convert_to_networks(azure_test_dataset)
         ip_start, ip_end = ip_ranges[0][0], ip_ranges[0][-1]
 
-        assert GreencheckIp.objects.all().count() == 0 # Test: database is empty 
-        hosting_provider.save() # Initialize hosting provider in database
+        assert GreencheckIp.objects.all().count() == 0  # Test: database is empty
+        hosting_provider.save()  # Initialize hosting provider in database
 
         # As the database is empty, create new range
         azure_cloud_provider.update_range_in_db(hosting_provider, ip_start, ip_end)
-        
-        assert GreencheckIp.objects.all().count() == 1 # Test: contains one value after insertion
+
+        assert (
+            GreencheckIp.objects.all().count() == 1
+        )  # Test: contains one value after insertion
 
     def test_extract_ip_ranges(
         self, hosting_provider, azure_cloud_provider, azure_test_dataset
@@ -93,8 +98,8 @@ class TestAZURECLoudImporter:
         """
         Test extracting ip ranges from dataset and saving in dataset.
         """
-        assert GreencheckIp.objects.all().count() == 0 # Test: database is empty 
-        hosting_provider.save() # Initialize hosting provider in database
+        assert GreencheckIp.objects.all().count() == 0  # Test: database is empty
+        hosting_provider.save()  # Initialize hosting provider in database
 
         ip_ranges = azure_cloud_provider.extract_ip_ranges(azure_test_dataset)
         ipv4s = ip_ranges["ipv4"]
@@ -105,7 +110,7 @@ class TestAZURECLoudImporter:
         assert len(ipv6s) == 146
 
         # Test: database has equal altercation in comparison to the returned dataset
-        # This value should equate to 795 in total 
+        # This value should equate to 795 in total
         assert GreencheckIp.objects.all().count() == len(ipv4s) + len(ipv6s)
 
 
