646e01b39b732335797d12e8879698b98949b8ec
Chris Adams <chris@productscience.co.uk>
2022-05-07 22:19:21 +0200

    use mocked values in management command test
    We mock out the HTTP network call to return our
    fixture value instead.

--- apps/greencheck/tests/test_equinix_cloud_importer.py
+++ apps/greencheck/tests/test_equinix_cloud_importer.py
@@ -117,7 +117,30 @@ class TestEquinixImportCommand:
     We _could_ mock the call to fetch ip ranges, if this turns out to be a slow test.
     """
 
-    def test_handle(self, equinix_cloud_provider):
+    def test_handle(self, mocker, equinix_test_dataset, equinix_cloud_provider):
+
+        # mock the call to `retrieve_dataset`, and return
+        # our local text file instead of making a
+        # network request. This gives consistent
+        # input and output we can check for in our assertions
+
+        # identify the method we want to modify to return a new
+        # value for
+        path_to_mock = (
+            "apps.greencheck.management.commands."
+            "update_equinix_ip_ranges.EquinixCloudProvider."
+            "retrieve_dataset"
+        )
+
+        # define the mock return value to call when
+        # the `retrieve_dataset` method is called, instead
+        mocker.patch(
+            path_to_mock, return_value=equinix_test_dataset,
+        )
+
         out = StringIO()
         call_command("update_equinix_ip_ranges", stdout=out)
         assert "Import Complete:" in out.getvalue()
+        assert "21 new IPV4 networks" in out.getvalue()
+        assert "13 IPV6 networks" in out.getvalue()
+        assert "15 AS Networks" in out.getvalue()
