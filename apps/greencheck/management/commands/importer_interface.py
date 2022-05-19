0a760ad33ad4cb4f840e955509ba0fc4405c3976
Roald Teunissen <Roaldteunissen@gmail.com>
2022-05-19 17:57:08 +0000

    Implemented a protocol based structure for the importer and added Equinix as example

---
+++ apps/greencheck/management/commands/importer_interface.py
@@ -0,0 +1,76 @@
+import ipaddress
+import re
+import logging
+
+from typing import Protocol, runtime_checkable
+from abc import abstractmethod
+from dataclasses import dataclass
+
+from apps.greencheck.models import GreencheckIp, GreencheckASN
+from apps.accounts.models import Hostingprovider
+
+logger = logging.getLogger(__name__)
+
+@runtime_checkable
+class Importer(Protocol):
+    def fetch_data_from_source(cls) -> list:
+        raise NotImplementedError
+
+    def parse_to_list(cls) -> list:
+        raise NotImplementedError
+
+class BaseImporter():
+    def process_addresses(cls, list_of_addresses: list):
+        # Determine the type of address (IPv4, IPv6 or ASN)for address in list_of_addresses:
+        try:
+            for address in list_of_addresses:
+                if re.search("(AS)[0-9]+$", address):
+                    # Address is ASN
+                    cls.save_asn(address)
+                elif isinstance(
+                    ipaddress.ip_network(address),
+                    (ipaddress.IPv4Network, ipaddress.IPv6Network),
+                ):
+                    # Address is IPv4 or IPv6
+                    cls.save_ip(address)
+        except ValueError:
+            logger.exception(
+                "Value has invalid structure. Must be IPv4 or IPv6 with subnetmask (101.102.103.104/27) or AS number (AS123456)."
+            )
+        except Exception:
+            logger.exception("Something really unexpected happened. Aborting")
+
+    def save_asn(cls, address: str):
+        hoster = Hostingprovider.objects.get(
+            pk=cls.hosting_provider_id
+        )  # TODO: Need a way to fetch the ID from subclasses here
+
+        gc_asn, created = GreencheckASN.objects.update_or_create(
+            active=True, asn=int(address.replace("AS", "")), hostingprovider=hoster
+        )
+        gc_asn.save()  # Save the newly created or updated object
+
+        if created:
+            # Only log and return when a new object was created
+            logger.debug(gc_asn)
+            return gc_asn
+
+    def save_ip(cls, address: str):
+        # Convert to IPv4 network with it's respective range
+        network = ipaddress.ip_network(address)
+        hoster = Hostingprovider.objects.get(
+            pk=cls.hosting_provider_id
+        )  # TODO: Need a way to fetch the ID from subclasses here
+
+        gc_ip, created = GreencheckIp.objects.update_or_create(
+            active=True, ip_start=network[1], ip_end=network[-1], hostingprovider=hoster
+        )
+        gc_ip.save()  # Save the newly created or updated object
+
+        if created:
+            # Only log and return when a new object was created
+            logger.debug(gc_ip)
+            return gc_ip
+
+
+# TODO: Implement a command handle for testing this interface
--- apps/greencheck/tests/test_equinix_importer.py
+++ apps/greencheck/tests/test_equinix_importer.py
@@ -3,29 +3,37 @@ import pathlib
 from io import StringIO
 
 from django.core.management import call_command
-from apps.greencheck.management.commands import update_equinix_ip_ranges
-from apps.accounts.models import Hostingprovider
-from apps.greencheck.models import GreencheckIp, GreencheckASN
+from apps.greencheck.management.commands.equinix_importer import EquinixImporter
 
 from django.conf import settings
 
 
 @pytest.fixture
-def equinix_test_dataset():
+def sample_data():
     """
     Retrieve a locally saved sample from the population as dataset to use for this test
-    Return: list format of the test dataset
+    Return: str (contents of the text file)
     """
     this_file = pathlib.Path(__file__)
     path = this_file.parent.parent.joinpath("fixtures", "equinix_dataset.txt")
 
-    list_of_ips = []
-    with open(path) as file:
-        for line in file.readlines():
-            if line.startswith("AS") or line[0].isdigit():
-                list_of_ips.append(line.split(" ", 1)[0])
+    return open(path).read()
+    
+@pytest.mark.django_db
+class TestEquinixImporter:
+    def test_parse_to_list(self, hosting_provider, sample_data):
+        """
+        Test the parsing function.
+        """
+        # Initialize Equinix importer
+        importer = EquinixImporter()
 
-    return list_of_ips
+        # Run parse list with sample data
+        list_of_addresses = importer.parse_to_list(sample_data)
+
+        # Test: resulting list contains items
+        assert len(list_of_addresses) > 0
+
 
 
 @pytest.mark.django_db
@@ -35,7 +43,7 @@ class TestEquinixImportCommand:
     We _could_ mock the call to fetch ip ranges, if this turns out to be a slow test.
     """
 
-    def test_handle(self, mocker, equinix_test_dataset):
+    def test_handle(self, mocker, sample_data):
 
         # mock the call to `retrieve_dataset`, and return
         # our local text file instead of making a
@@ -45,20 +53,17 @@ class TestEquinixImportCommand:
         # identify the method we want to modify to return a new
         # value for
         path_to_mock = (
-            "apps.greencheck.management.commands.importers."
+            "apps.greencheck.management.commands."
             "equinix_importer.EquinixImporter."
-            "fetch_data"
+            "fetch_data_from_source"
         )
 
         # define the mock return value to call when
         # the `retrieve_dataset` method is called, instead
         mocker.patch(
-            path_to_mock, return_value=equinix_test_dataset,
+            path_to_mock, return_value=sample_data,
         )
 
         out = StringIO()
         call_command("equinix_importer", stdout=out)
-        assert "Import Complete:" in out.getvalue()
-        assert "21 new IPV4 networks" in out.getvalue()
-        assert "13 IPV6 networks" in out.getvalue()
-        assert "15 AS Networks" in out.getvalue()
+        # TODO: Report back on the output
