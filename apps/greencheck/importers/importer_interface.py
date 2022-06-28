18fe1e298f3b606fbdfbe25d9d7d20564bf0d633
Roald Teunissen <Roaldteunissen@gmail.com>
2022-06-28 16:27:56 +0000

    Initial draft for a solution to update all entries associated to a hosting provider

--- apps/greencheck/importers/importer_interface.py
+++ apps/greencheck/importers/importer_interface.py
@@ -1,13 +1,16 @@
 import ipaddress
 import re
 import logging
+import pdb
 
-from typing import Protocol, runtime_checkable
+from typing import Protocol, runtime_checkable, List
+from django.db import transaction
 
 from apps.greencheck.models import GreencheckIp, GreencheckASN
 from apps.accounts.models import Hostingprovider
 
 logger = logging.getLogger(__name__)
+
 
 @runtime_checkable
 class Importer(Protocol):
@@ -17,67 +20,162 @@ class Importer(Protocol):
     def parse_to_list(cls) -> list:
         raise NotImplementedError
 
-class BaseImporter():
-    hosting_provider_id:int
 
+class BaseImporter:
+    hosting_provider_id: int
+
+    # @transaction.atomic
     def process_addresses(cls, list_of_addresses: list):
-        count_asn = 0
         count_ip = 0
-        
-        # Determine the type of address (IPv4, IPv6 or ASN)for address in list_of_addresses:
+        count_asn = 0
+
         try:
-            for address in list_of_addresses:
-                if re.search("(AS)[0-9]+$", address):
-                    # Address is ASN
-                    cls.save_asn(cls, address)
-                    count_asn += 1
-                elif isinstance(
-                    ipaddress.ip_network(address),
-                    (ipaddress.IPv4Network, ipaddress.IPv6Network),
-                ):
-                    # Address is IPv4 or IPv6
-                    cls.save_ip(cls, address)
-                    count_ip += 1
-            
-            return f"Processing complete. Added {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
+            # Prepare ASN and IP lists
+            regex = re.compile("(AS)[0-9]+$")
+            asn_list = list(filter(regex.match, list_of_addresses))
+            ip_list = list(set(list_of_addresses) - set(asn_list))
+
+            # Final ASN preparation and run updater
+            asn_list = list(map(lambda x: x.replace("AS", ""), asn_list))
+            count_asn = cls.update_asn(cls, asn_list)
+
+            # Final IP preparation and run updater
+            ip_list = list(
+                filter(
+                    lambda x: isinstance(
+                        ipaddress.ip_network(x),
+                        (ipaddress.IPv4Network, ipaddress.IPv6Network),
+                    ),
+                    ip_list,
+                )
+            )
+            count_ip = cls.update_ip(cls, ip_list)
+
+            return f"Processing complete. Updated {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
         except ValueError:
             logger.exception(
                 "Value has invalid structure. Must be IPv4 or IPv6 with subnetmask (101.102.103.104/27) or AS number (AS123456)."
             )
-            return f"An error occurred while adding new entries. Added {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
+            return f"An error occurred while adding new entries. Updated {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
         except Exception as e:
             logger.exception("Something really unexpected happened. Aborting")
             logger.exception(e)
-            return f"An error occurred while adding new entries. Added {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
+            return f"An error occurred while adding new entries. Updated {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
 
-    def save_asn(cls, address: str):
-        hoster = Hostingprovider.objects.get(
-            pk=cls.hosting_provider_id
+    def update_asn(cls, active_addresses: List[str]):
+        updated_addresses = 0
+        hosting_provider = Hostingprovider.objects.get(pk=cls.hosting_provider_id)
+
+        # Retrieve all ASN entries assosiated to this hosting provider
+        entries = GreencheckASN.objects.select_for_update().filter(
+            hostingprovider=hosting_provider
         )
 
-        gc_asn, created = GreencheckASN.objects.update_or_create(
-            active=True, asn=int(address.replace("AS", "")), hostingprovider=hoster
+        logger.debug("Running atomic database transaction. Altered AS numbers:")
+        with transaction.atomic():
+            # Iterate database entries
+            for entry in entries:
+                if entry.asn in active_addresses:
+                    entry.active = True
+                    entry.save()
+
+                    updated_addresses += 1
+                    logger.debug(entry)
+                elif entry.active == True:
+                    entry.active = False
+                    entry.save()
+
+                    updated_addresses += 1
+                    logger.debug(entry)
+
+            # # Iterate remaining list items (that are not in the database)
+            # if active_addresses:
+            #     for address in active_addresses:
+            #         # # TODO: FOLLOWING NEEDS SOME WORK. 
+            #         # OPTION 1
+            #         entry, created = GreencheckASN.objects.update_or_create(
+            #             active=True, asn=address, hostingprovider=hosting_provider
+            #         )
+            #         entry.save()
+
+            #         if created:
+            #             logger.debug(entry)
+
+            #         # OPTION 2
+            #         entry = GreencheckASN.objects.filter(hostingprovider=hosting_provider, asn=address).first()
+            #         if entry == False:
+            #             entry.active = True
+            #             entry.save()
+
+            #             updated_addresses += 1
+            #             logger.debug(entry)
+
+        return updated_addresses
+
+    def update_ip(cls, active_addresses: List[str]):
+        updated_addresses = 0
+
+        # Convert list of networks (ip with subnet) to list of
+        # start and end ip addresses.
+        tmp_list = []
+        for address in active_addresses:
+            network = ipaddress.ip_network(address)
+            tmp_list.append((str(network[1]), str(network[-1])))
+        active_addresses = tmp_list
+
+        # Retrieve all IP(v4 and v6) entries assosiated to this hosting provider
+        hosting_provider = Hostingprovider.objects.get(pk=cls.hosting_provider_id)
+        entries = GreencheckIp.objects.select_for_update().filter(
+            hostingprovider=hosting_provider
         )
-        gc_asn.save()  # Save the newly created or updated object
 
-        if created:
-            # Only log and return when a new object was created
-            logger.debug(gc_asn)
-            return gc_asn
+        logger.debug("Running atomic database transaction. Altered IP ranges:")
+        with transaction.atomic():
+            # Iterate database entries
+            for entry in entries:
+                if (entry.ip_start, entry.ip_end) in active_addresses:
+                    active_addresses.pop(
+                        active_addresses.index((entry.ip_start, entry.ip_end))
+                    )
+                    entry.active = True
+                    entry.save()
 
-    def save_ip(cls, address: str):
-        # Convert to IPv4 network with it's respective range
-        network = ipaddress.ip_network(address)
-        hoster = Hostingprovider.objects.get(
-            pk=cls.hosting_provider_id
-        )
+                    updated_addresses += 1
+                    logger.debug(entry)
+                elif entry.active == True:
+                    entry.active = False
+                    entry.save()
 
-        gc_ip, created = GreencheckIp.objects.update_or_create(
-            active=True, ip_start=network[1], ip_end=network[-1], hostingprovider=hoster
-        )
-        gc_ip.save()  # Save the newly created or updated object
+                    updated_addresses += 1
+                    logger.debug(entry)
 
-        if created:
-            # Only log and return when a new object was created
-            logger.debug(gc_ip)
-            return gc_ip
+            # Iterate remaining list items (that are not in the database)
+            # if active_addresses:
+            #     for address in active_addresses:
+                    # # TODO: FOLLOWING NEEDS SOME WORK. 
+                    # # OPTION 1
+                    # entry, created = GreencheckIp.objects.update_or_create(
+                    #     active=True,
+                    #     ip_start=address[0],
+                    #     ip_end=address[1],
+                    #     hostingprovider=hosting_provider,
+                    # )
+                    # entry.save()
+
+                    # if created:
+                    #     logger.debug(entry)
+
+                    # # OPTION 2
+                    # entry = GreencheckIp.objects.filter(hostingprovider=hosting_provider, 
+                    #     ip_start=address[0],
+                    #     ip_end=address[1]).first()
+                        
+                    # pdb.set_trace()
+                    # if entry.active == False:
+                    #     entry.active = True
+                    #     entry.save()
+
+                    #     updated_addresses += 1
+                    #     logger.debug(entry)
+
+        return updated_addresses
--- apps/greencheck/tests/test_base_importer.py
+++ apps/greencheck/tests/test_base_importer.py
@@ -2,6 +2,7 @@ import pytest
 import pathlib
 import json
 from io import StringIO
+import pdb
 
 from django.core.management import call_command
 from apps.greencheck.importers.importer_interface import BaseImporter
@@ -31,6 +32,15 @@ def hosting_provider():
         website="",
     )
 
+@pytest.fixture
+def asn_list():
+    """
+    Define a list of Autonomous System(AS) Numbers for testing.
+    Return: a list of ASN's
+    """
+
+
+
 
 @pytest.fixture
 def base_importer():
@@ -59,49 +69,49 @@ def sample_data():
 
 @pytest.mark.django_db
 class TestImporterInterface:
-    def test_save_ip(self, hosting_provider, base_importer):
-        """
-        Test saving IPv4 and IPv6 networks to the database
-        """
-        testing_ipv4 = "191.233.8.24/29"
-        testing_ipv6 = "2603:1010:304::140/123"
+    # def test_save_ip(self, hosting_provider, base_importer):
+    #     """
+    #     Test saving IPv4 and IPv6 networks to the database
+    #     """
+    #     testing_ipv4 = "191.233.8.24/29"
+    #     testing_ipv6 = "2603:1010:304::140/123"
 
-        assert (
-            GreencheckIp.objects.all().count() == 0
-        )  # Test: database is empty (for IP)
-        hosting_provider.save()  # Initialize hosting provider in database
+    #     assert (
+    #         GreencheckIp.objects.all().count() == 0
+    #     )  # Test: database is empty (for IP)
+    #     hosting_provider.save()  # Initialize hosting provider in database
 
-        # Import a single IPv4 network
-        BaseImporter.save_ip(base_importer, testing_ipv4)
+    #     # Import a single IPv4 network
+    #     BaseImporter.save_ip(base_importer, testing_ipv4)
 
-        assert (
-            GreencheckIp.objects.all().count() == 1
-        )  # Test: IPv4 is saved after insertion
+    #     assert (
+    #         GreencheckIp.objects.all().count() == 1
+    #     )  # Test: IPv4 is saved after insertion
 
-        # Import a single IPv6 network
-        BaseImporter.save_ip(base_importer, testing_ipv6)
+    #     # Import a single IPv6 network
+    #     BaseImporter.save_ip(base_importer, testing_ipv6)
 
-        assert (
-            GreencheckIp.objects.all().count() == 2
-        )  # Test: IPv6 is saved after insertion
+    #     assert (
+    #         GreencheckIp.objects.all().count() == 2
+    #     )  # Test: IPv6 is saved after insertion
 
-    def test_save_asn(self, hosting_provider, base_importer):
-        """
-        Test saving Autonomous System Numbers (ASN) networks to the database
-        """
-        testing_asn = "AS27407"
+    # def test_save_asn(self, hosting_provider, base_importer):
+    #     """
+    #     Test saving Autonomous System Numbers (ASN) networks to the database
+    #     """
+    #     testing_asn = "AS27407"
 
-        assert (
-            GreencheckASN.objects.all().count() == 0
-        )  # Test: database is empty (for ASN)
-        hosting_provider.save()  # Initialize hosting provider in database
+    #     assert (
+    #         GreencheckASN.objects.all().count() == 0
+    #     )  # Test: database is empty (for ASN)
+    #     hosting_provider.save()  # Initialize hosting provider in database
 
-        # Import a single ASN network
-        BaseImporter.save_asn(base_importer, testing_asn)
+    #     # Import a single ASN network
+    #     BaseImporter.save_asn(base_importer, testing_asn)
 
-        assert (
-            GreencheckASN.objects.all().count() == 1
-        )  # Test: ASN is saved after insertion
+    #     assert (
+    #         GreencheckASN.objects.all().count() == 1
+    #     )  # Test: ASN is saved after insertion
 
     def test_process_addresses(self, hosting_provider, base_importer, sample_data):
         """
@@ -116,8 +126,31 @@ class TestImporterInterface:
         )  # Test: database is empty (for ASN)
         hosting_provider.save()  # Initialize hosting provider in database
 
+        # TODO: This can be replaced by having a some dummy data imported beforehand
+        # ASN dummy database data
+        GreencheckASN.objects.bulk_create([
+            GreencheckASN(hostingprovider=hosting_provider, asn='29154', active=True),
+            GreencheckASN(hostingprovider=hosting_provider, asn='27466', active=False),
+            GreencheckASN(hostingprovider=hosting_provider, asn='270119', active=True),
+            GreencheckASN(hostingprovider=hosting_provider, asn='27566', active=False),
+            GreencheckASN(hostingprovider=hosting_provider, asn='270132', active=True),
+            GreencheckASN(hostingprovider=hosting_provider, asn='230132', active=True),
+        ])
+
+        # IP dummy database data
+        GreencheckIp.objects.bulk_create([
+            GreencheckIp(hostingprovider=hosting_provider, active=False, ip_start="40.80.168.113", ip_end="40.80.168.119",),
+            GreencheckIp(hostingprovider=hosting_provider, active=False, ip_start="18.208.0.1", ip_end="18.215.255.255",),
+            GreencheckIp(hostingprovider=hosting_provider, active=True, ip_start="2603:1040:a06:1::141", ip_end="2603:1040:a06:1::15f",),
+            GreencheckIp(hostingprovider=hosting_provider, active=True, ip_start="2603:1020:605::141", ip_end="2603:1020:605::15f",),
+            GreencheckIp(hostingprovider=hosting_provider, active=False, ip_start="2603:1020:705:1::141", ip_end="2603:1020:705:1::15f",),
+            GreencheckIp(hostingprovider=hosting_provider, active=False, ip_start="43.81.168.113", ip_end="43.81.168.119",),
+        ])
+
         # Process list of addresses in JSON file
         BaseImporter.process_addresses(base_importer, sample_data)
+
+        all_asn = GreencheckASN.objects.all()
 
         assert (
             GreencheckIp.objects.all().count() == 63
