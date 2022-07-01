3394928fcbf3c2c9b94afbf41af2b24d1f78e62a
Roald Teunissen <Roaldteunissen@gmail.com>
2022-07-01 12:12:41 +0000

    Implemented a working ASN and IP updater in importer_interface

--- apps/greencheck/importers/importer_interface.py
+++ apps/greencheck/importers/importer_interface.py
@@ -30,16 +30,16 @@ class BaseImporter:
         count_asn = 0
 
         try:
-            # Prepare ASN and IP lists
+            # Extract ASN and IP values from the generic list
             regex = re.compile("(AS)[0-9]+$")
             asn_list = list(filter(regex.match, list_of_addresses))
             ip_list = list(set(list_of_addresses) - set(asn_list))
 
-            # Final ASN preparation and run updater
-            asn_list = list(map(lambda x: x.replace("AS", ""), asn_list))
+            # ASN list preperation: formatting
+            asn_list = list(map(int, map(lambda x: x.replace("AS", ""), asn_list)))
             count_asn = cls.update_asn(cls, asn_list)
 
-            # Final IP preparation and run updater
+            # IP list preperation: type checking
             ip_list = list(
                 filter(
                     lambda x: isinstance(
@@ -51,131 +51,107 @@ class BaseImporter:
             )
             count_ip = cls.update_ip(cls, ip_list)
 
-            return f"Processing complete. Updated {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
+            return f"Processing complete. Updated {count_asn} ASN's and {count_ip} IP's (IPv4 and/or IPv6)"
         except ValueError:
             logger.exception(
                 "Value has invalid structure. Must be IPv4 or IPv6 with subnetmask (101.102.103.104/27) or AS number (AS123456)."
             )
-            return f"An error occurred while adding new entries. Updated {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
+            return f"An error occurred while adding new entries. Updated {count_asn} ASN's and {count_ip} IP's (IPv4 and/or IPv6)"
         except Exception as e:
             logger.exception("Something really unexpected happened. Aborting")
             logger.exception(e)
-            return f"An error occurred while adding new entries. Updated {count_asn} ASN's and {count_ip} IP's (either IPv4 and/or IPv6)"
+            return f"An error occurred while adding new entries. Updated {count_asn} ASN's and {count_ip} IP's (IPv4 and/or IPv6)"
 
-    def update_asn(cls, active_addresses: List[str]):
-        updated_addresses = 0
-        hosting_provider = Hostingprovider.objects.get(pk=cls.hosting_provider_id)
-
-        # Retrieve all ASN entries assosiated to this hosting provider
-        entries = GreencheckASN.objects.select_for_update().filter(
-            hostingprovider=hosting_provider
-        )
+    def update_asn(cls, active_networks: List[int]) -> int:
+        active_networks = list(set(active_networks))  # extract unique values
+        updated_networks = 0
 
         logger.debug("Running atomic database transaction. Altered AS numbers:")
         with transaction.atomic():
+            # Retrieve all ASN entries assosiated with this hosting provider
+            hosting_provider = Hostingprovider.objects.get(pk=cls.hosting_provider_id)
+            entries = GreencheckASN.objects.select_for_update().filter(
+                hostingprovider=hosting_provider
+            )
+
             # Iterate database entries
             for entry in entries:
-                if entry.asn in active_addresses:
+                if entry.asn in active_networks:
+                    active_networks.pop(active_networks.index(entry.asn))
                     entry.active = True
                     entry.save()
 
-                    updated_addresses += 1
+                    updated_networks += 1
                     logger.debug(entry)
                 elif entry.active == True:
                     entry.active = False
                     entry.save()
 
-                    updated_addresses += 1
+                    updated_networks += 1
                     logger.debug(entry)
 
-            # # Iterate remaining list items (that are not in the database)
-            # if active_addresses:
-            #     for address in active_addresses:
-            #         # # TODO: FOLLOWING NEEDS SOME WORK. 
-            #         # OPTION 1
-            #         entry, created = GreencheckASN.objects.update_or_create(
-            #             active=True, asn=address, hostingprovider=hosting_provider
-            #         )
-            #         entry.save()
+            # Iterate remaining list items (that are not in the database)
+            if active_networks:
+                for asn_network in active_networks:
+                    entry, created = GreencheckASN.objects.update_or_create(
+                        active=True, asn=asn_network, hostingprovider=hosting_provider
+                    )
+                    entry.save()
 
-            #         if created:
-            #             logger.debug(entry)
+                    if created:
+                        logger.debug(entry)
 
-            #         # OPTION 2
-            #         entry = GreencheckASN.objects.filter(hostingprovider=hosting_provider, asn=address).first()
-            #         if entry == False:
-            #             entry.active = True
-            #             entry.save()
+        return updated_networks
 
-            #             updated_addresses += 1
-            #             logger.debug(entry)
+    def update_ip(cls, active_networks: List[str]) -> int:
+        updated_networks = 0
 
-        return updated_addresses
-
-    def update_ip(cls, active_addresses: List[str]):
-        updated_addresses = 0
-
-        # Convert list of networks (ip with subnet) to list of
-        # start and end ip addresses.
+        # Convert networks (xxx.xxx.xxx.xxx/yy) to a
+        # start and end ip address
         tmp_list = []
-        for address in active_addresses:
-            network = ipaddress.ip_network(address)
+        for ip_network in active_networks:
+            network = ipaddress.ip_network(ip_network)
             tmp_list.append((str(network[1]), str(network[-1])))
-        active_addresses = tmp_list
-
-        # Retrieve all IP(v4 and v6) entries assosiated to this hosting provider
-        hosting_provider = Hostingprovider.objects.get(pk=cls.hosting_provider_id)
-        entries = GreencheckIp.objects.select_for_update().filter(
-            hostingprovider=hosting_provider
-        )
+        active_networks = list(set(tmp_list))
 
         logger.debug("Running atomic database transaction. Altered IP ranges:")
         with transaction.atomic():
+            # Retrieve all IP(v4 and v6) entries assosiated to this hosting provider
+            hosting_provider = Hostingprovider.objects.get(pk=cls.hosting_provider_id)
+            entries = GreencheckIp.objects.select_for_update().filter(
+                hostingprovider=hosting_provider
+            )
+
             # Iterate database entries
             for entry in entries:
-                if (entry.ip_start, entry.ip_end) in active_addresses:
-                    active_addresses.pop(
-                        active_addresses.index((entry.ip_start, entry.ip_end))
+                if (entry.ip_start, entry.ip_end) in active_networks:
+                    active_networks.pop(
+                        active_networks.index((entry.ip_start, entry.ip_end))
                     )
                     entry.active = True
                     entry.save()
 
-                    updated_addresses += 1
+                    updated_networks += 1
                     logger.debug(entry)
                 elif entry.active == True:
                     entry.active = False
                     entry.save()
 
-                    updated_addresses += 1
+                    updated_networks += 1
                     logger.debug(entry)
 
             # Iterate remaining list items (that are not in the database)
-            # if active_addresses:
-            #     for address in active_addresses:
-                    # # TODO: FOLLOWING NEEDS SOME WORK. 
-                    # # OPTION 1
-                    # entry, created = GreencheckIp.objects.update_or_create(
-                    #     active=True,
-                    #     ip_start=address[0],
-                    #     ip_end=address[1],
-                    #     hostingprovider=hosting_provider,
-                    # )
-                    # entry.save()
+            if active_networks:
+                for ip_network in active_networks:
+                    entry, created = GreencheckIp.objects.update_or_create(
+                        active=True,
+                        ip_start=ip_network[0],
+                        ip_end=ip_network[1],
+                        hostingprovider=hosting_provider,
+                    )
+                    entry.save()
 
-                    # if created:
-                    #     logger.debug(entry)
+            if created:
+                logger.debug(entry)
 
-                    # # OPTION 2
-                    # entry = GreencheckIp.objects.filter(hostingprovider=hosting_provider, 
-                    #     ip_start=address[0],
-                    #     ip_end=address[1]).first()
-                        
-                    # pdb.set_trace()
-                    # if entry.active == False:
-                    #     entry.active = True
-                    #     entry.save()
-
-                    #     updated_addresses += 1
-                    #     logger.debug(entry)
-
-        return updated_addresses
+        return updated_networks
