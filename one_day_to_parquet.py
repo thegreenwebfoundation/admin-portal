31a976588acf4449374d216924dcc6a99b4085ba
Chris Adams <chris@productscience.co.uk>
2022-09-11 01:53:32 +0200

    Refactor greencheck parquet upload (#305)
    * Simplify upload scripts for big greencheck tables
    
    * Fix function signature

--- one_day_to_parquet.py
+++ one_day_to_parquet.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 infra_bucket = object_storage_bucket("internal-infra")
 
 
-def csv_of_checks_for_day(day=datetime.date, directory: str = None) -> bool:
+def csv_of_checks_for_day(day: datetime.date, csv_path: str) -> bool:
     """
     Run a query extracting the checks for the given day, and write the results
     date stamped csv file .
@@ -27,7 +27,6 @@ def csv_of_checks_for_day(day=datetime.date, directory: str = None) -> bool:
     logger.info(f"Starting export for {day}")
     start_time = timezone.now()
 
-    date_string = day.strftime("%Y-%m_%d")
     one_day_forward = timedelta(days=1)
     one_day_back = timedelta(days=-1)
 
@@ -38,10 +37,6 @@ def csv_of_checks_for_day(day=datetime.date, directory: str = None) -> bool:
         .values_list()
         .iterator()
     )
-
-    csv_path = f"local_file_{date_string}.csv"
-    if directory is not None:
-        csv_path = pathlib.Path(directory).joinpath(csv_path)
 
     logger.info(f"Writing query results to {csv_path}")
 
@@ -61,38 +56,30 @@ def csv_of_checks_for_day(day=datetime.date, directory: str = None) -> bool:
     return True
 
 
-def convert_csv_to_parquet(day=datetime.date, directory: str = None) -> bool:
+def convert_csv_to_parquet(csv_path: str, parquet_path: str) -> bool:
     """
-    Load a CSV file at the path of `local_file_YYYY_MM_DD.csv`, and
+    Load a CSV file at the path of `prefix/YYYY_MM_DD.csv`, and
     create a compressed parquet file at the path
-    `local_file_YYYY_MM_DD.zstd.parquet`.
+    `prefix/YYYY_MM_DD.zstd.parquet`.
     If a directory is given, look inside it for the csv, and output
     the parquet file into it.
     """
 
     start_time = timezone.now()
-    logger.info(f"Starting conversion to parquet for {day}")
+    logger.info(f"Starting conversion to parquet for {csv_path}")
     logger.info(start_time)
 
-    date_string = day.strftime("%Y-%m_%d")
     # set up our in-memory database with duckdb
     con = duckdb.connect(database=":memory:")
-
-    local_parquet_path = f"local_file_{date_string}.zstd.parquet"
-    csv_path = f"local_file_{date_string}.csv"
-
-    if directory is not None:
-        local_parquet_path = str(pathlib.Path(directory).joinpath(local_parquet_path))
-        csv_path = str(pathlib.Path(directory).joinpath(csv_path))
 
     # run our query to convert the day of checks to a compressed parquet file
     con.execute(
         (
             f"COPY (SELECT * from '{csv_path}') "
-            f"TO '{local_parquet_path}' (FORMAT 'PARQUET', CODEC 'ZSTD')"
+            f"TO '{parquet_path}' (FORMAT 'PARQUET', CODEC 'ZSTD')"
         )
     )
-    logger.info(f"Finished conversion to parquet {day}")
+    logger.info(f"Finished conversion to parquet for {csv_path}")
     end_time = timezone.now()
     logger.info(end_time)
 
@@ -100,27 +87,17 @@ def convert_csv_to_parquet(day=datetime.date, directory: str = None) -> bool:
     logger.info(f"Took {time_span.seconds} seconds")
 
 
-def upload_to_object_storage(day=datetime.date, directory: str = None) -> bool:
+def upload_to_object_storage(parquet_path: str, upload_path: str) -> bool:
     """
-    Upload parquet file at `"local_file_YYYY_MM_DD.zstd.parquet" to object storage
+    Upload parquet file at `"prefix/YYYY_MM_DD.zstd.parquet" to object storage
     with the the key /parquet/days/TABLENAME_YYYY_MM_DD.zstd.parquet
 
     If a directory is provided, look in the directory for the parquet file.
     """
-    date_string = day.strftime("%Y-%m_%d")
-    greencheck_table = "greencheck_2021"
-
-    parquet_file_path = f"local_file_{date_string}.zstd.parquet"
-    prefix = "parquet/days/"
-    upload_path = f"{prefix}/{date_string}.{greencheck_table}.zstd.parquet"
-
-    if directory is not None:
-        parquet_file_path = str(pathlib.Path(directory).joinpath(parquet_file_path))
-
     start_time = timezone.now()
     logger.info(f"Start uploading at {start_time}")
 
-    infra_bucket.upload_file(parquet_file_path, upload_path)
+    infra_bucket.upload_file(parquet_path, upload_path)
 
     end_time = timezone.now()
     time_span = end_time - start_time
@@ -139,6 +116,13 @@ def backup_day_to_parquet(target_date: datetime.date):
     # use a temporary directory, to clean up after ourselves
     # and avoid clashes
     with tempfile.TemporaryDirectory() as tmpdir:
-        csv_of_checks_for_day(target_date, tmpdir)
-        convert_csv_to_parquet(target_date, tmpdir)
-        upload_to_object_storage(target_date, tmpdir)
+        date_string = target_date.strftime("%Y-%m-%d")
+        greencheck_table = "greencheck_2021"
+
+        csv_path = f"{tmpdir}/{date_string}.local_file.csv"
+        parquet_path = f"{tmpdir}/{date_string}.zstd.parquet"
+        upload_path = f"parquet/days/{date_string}.{greencheck_table}.zstd.parquet"
+
+        csv_of_checks_for_day(target_date, csv_path)
+        convert_csv_to_parquet(csv_path, parquet_path)
+        upload_to_object_storage(parquet_path, upload_path)
