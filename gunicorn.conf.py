b72ad41f9296147301319774f7062fa16efea126
Chris Adams <chris@productscience.co.uk>
2022-10-04 07:15:47 +0200

    Second pass at refactoring how we serve requests

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,2 +1,7 @@
 import logging
 import os
+
+# increasing workers uses more RAM, but provides a simple model for scaling up resources
+workers = 2
+# increasing threads saves RAM at the cost of using more CPU
+threads = 3
