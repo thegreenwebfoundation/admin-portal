a376c4ffe0c636134527b38eaa0665ac3f200528
Chris Adams <chris@productscience.co.uk>
2022-10-18 13:37:20 +0200

    Increase the workers
    We have spare ram capacity in both worker machines, this lets us use it

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,6 @@ import logging
 import os
 
 # increasing workers uses more RAM, but provides a simple model for scaling up resources
-workers = 3
+workers = 8
 # increasing threads saves RAM at the cost of using more CPU
-threads = 3
+threads = 1
