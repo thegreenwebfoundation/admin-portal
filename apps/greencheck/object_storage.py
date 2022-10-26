fcadd784bf1705aef2ba0efe123a399cddf43bd0
Roald Teunissen <Roaldteunissen@gmail.com>
2022-10-26 15:19:42 +0000

    Split logic in updater, generalized object storage

--- apps/greencheck/object_storage.py
+++ apps/greencheck/object_storage.py
@@ -3,7 +3,8 @@ We use scaleway as a AWS S3 API compatible provider of object storage.
 Below is the wrapper around it for working with the objects.
 """
 import boto3
-import json
+import logging
+from botocore.exceptions import ClientError
 from django.conf import settings
 
 
@@ -26,8 +27,8 @@ def retrieve_object_storage():
 
 # TODO: how to specify this return time. The following example throws an error as
 # this structure does not exist
-# def retrieve_bucket(bucket_name: str) -> boto3.resources.factory.s3.Bucket:
-def retrieve_bucket(bucket_name: str):
+# def retrieve_specific_bucket(bucket_name: str) -> boto3.resources.factory.s3.Bucket:
+def retrieve_specific_bucket(bucket_name: str):
     """
     Retrieve specified bucket from our object storage resource.
     """
@@ -40,16 +41,26 @@ def bucket_green_domains():
     Retrieve the green domains bucket.
     This bucket contains snapshots of green domain tables
     """
-    return retrieve_bucket(settings.DOMAIN_SNAPSHOT_BUCKET)
+    return retrieve_specific_bucket(settings.DOMAIN_SNAPSHOT_BUCKET)
+
+# TODO: ask what Chris thinks of this new format used by Boto3 for docstrings:
 
 
-def upload_json_file_to_bucket(bucket_name: str, json_data: dict, file_name: str):
+def upload_file_to_bucket(object_data, object_name, bucket):
+    """Upload a file to an S3 bucket
+
+    :param object_data: S3 object data
+    :param object_name: S3 object name
+    :param bucket: Bucket to upload to
+    :return: True if file was uploaded, else False
     """
-    Upload a JSON file to the object storage.
-    The file will be overwritten if the file already exists
-    """
-    s3_object = retrieve_object_storage().Object(bucket_name, file_name)
-    s3_object.put(Body=(bytes(json.dumps(json_data).encode("UTF-8"))))
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.upload_fileobj(object_data, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 def public_url(bucket: str, key: str) -> str:
