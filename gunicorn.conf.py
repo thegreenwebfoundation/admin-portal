a5f436d517856327b6961029f6738c23dbc8f86e
Chris Adams <chris@productscience.co.uk>
2022-10-31 16:44:06 +0100

    Fix typo on gunicorn workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,6 @@ import logging
 import os
 
 # increasing workers uses more RAM, but provides a simple model for scaling up resources
-workers = os.getenv("GUNICORN_WORKERS," default=8)
+workers = os.getenv("GUNICORN_WORKERS", default=8)
 # increasing threads saves RAM at the cost of using more CPU
 threads = 1
