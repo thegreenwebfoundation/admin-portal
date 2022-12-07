ced09c457104bcdbb3e808e5123fc6589c12d53a
Oliwia Zaremba <git@olka.dev>
2022-12-07 15:20:15 +0100

    Test for ProviderRequest list view + factory

--- apps/accounts/tests/test_provider_request.py
+++ apps/accounts/tests/test_provider_request.py
@@ -1,12 +1,27 @@
 import pytest
 import io
+import factory
 
 from django import urls
 from django.core.exceptions import ValidationError
 from django.core.files import File
 from waffle.testutils import override_flag
+from factory.django import DjangoModelFactory
 
 from .. import models
+from apps.greencheck.factories import UserFactory, TagFactory
+
+
+class ProviderRequestFactory(DjangoModelFactory):
+    name = factory.Faker("word")
+    website = factory.Faker("domain_name")
+    description = factory.Faker("sentence")
+    status = models.ProviderRequestStatus.OPEN
+    created_by = factory.SubFactory(UserFactory)
+    services = factory.List([factory.SubFactory(TagFactory) for _ in range(3)])
+
+    class Meta:
+        model = models.ProviderRequest
 
 
 @pytest.fixture()
@@ -89,3 +104,33 @@ def test_detail_view_forbidden_for_others(
     response = client.get(url)
 
     assert response.status_code == 404
+
+
+@pytest.mark.django_db
+@override_flag("provider_request", active=True)
+def test_list_view_displays_only_authored_requests(client):
+    # given: 3 provider requests exist, created by different users
+    pr1 = ProviderRequestFactory.create()
+    pr2 = ProviderRequestFactory.create()
+    pr3 = ProviderRequestFactory.create()
+
+    # when: accessing the list view as the author of pr2
+    user = pr2.created_by
+    client.force_login(user)
+    url = urls.reverse("provider_request_list")
+    response = client.get(url)
+
+    # then: link to detail view of pr2 is displayed
+    assert response.status_code == 200
+    assert f'href="{pr2.get_absolute_url()}"'.encode() in response.content
+
+    # then: links to pr1 and pr3 are not displayed
+    assert f'href="{pr1.get_absolute_url()}"'.encode() not in response.content
+    assert f'href="{pr3.get_absolute_url()}"'.encode() not in response.content
+
+
+@pytest.mark.django_db
+@override_flag("provider_request", active=True)
+def test_wizard_view_happy_path(user, client):
+    # reference: https://marcosschroh.github.io/posts/testing-a-form-wizard-in-django/
+    pass
