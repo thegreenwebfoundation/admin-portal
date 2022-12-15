ec2f8b81fbb57917816687aaa42b3b2849ef2e6b
Chris Adams <chris@productscience.co.uk>
2022-12-15 17:23:25 +0100

    Update django tailwind to tailwind 3

--- apps/theme/static_src/tailwind.config.js
+++ apps/theme/static_src/tailwind.config.js
@@ -6,15 +6,8 @@
  */
 
 module.exports = {
-    /**
-     * Stylesheet generation mode.
-     *
-     * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
-     * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
-     */
-    mode: "jit",
 
-    purge: [
+    content: [
         /**
          * HTML. Paths to Django template files that will contain Tailwind CSS classes.
          */
@@ -42,7 +35,6 @@ module.exports = {
          */
         // '../../**/*.py'
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
