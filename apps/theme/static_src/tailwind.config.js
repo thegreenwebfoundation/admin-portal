1de11d2ec05673f60a0bd7971d3be217dea27cee
Chris Adams <chris@productscience.co.uk>
2022-12-15 18:39:44 +0100

    Set up for later styling

--- apps/theme/static_src/tailwind.config.js
+++ apps/theme/static_src/tailwind.config.js
@@ -53,14 +53,10 @@ module.exports = {
         },
     },
     plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('daisyui')
     ],
 }
