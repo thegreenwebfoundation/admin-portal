6c1a14164f0136651b7be7e4a2bfd95070f95e4d
Oliwia Zaremba <git@olka.dev>
2023-04-12 16:39:21 +0200

    Configure password reset views

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,8 +7,6 @@ from apps.accounts.admin_site import greenweb_admin as admin
 from apps.accounts.views import (
     UserActivationView,
     UserRegistrationView,
-    UserLoginView,
-    UserLogoutView,
     DashboardView,
     UserUpdateView,
     ProviderRequestListView,
@@ -31,17 +29,50 @@ urlpatterns = [
     path("admin/", admin.urls),
     # auth pages
     path(
-        "password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
+        "accounts/signup/",
+        UserRegistrationView.as_view(template_name="auth/registration.html"),
+        name="registration",
     ),
-    path("accounts/signup/", UserRegistrationView.as_view(), name="registration"),
-    path("accounts/login/", UserLoginView.as_view(), name="login"),
-    path("accounts/logout/", UserLogoutView.as_view(), name="logout"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="auth/login.html"),
+        name="login",
+    ),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(template_name="auth/logout.html"),
+        name="logout",
+    ),
     url(
         r"accounts/activation/(?P<activation_key>[-:\w]+)/",
         UserActivationView.as_view(),
         name="activation",
+    ),
+    path(
+        "accounts/password_reset/",
+        auth_views.PasswordResetView.as_view(template_name="auth/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "accounts/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="auth/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "accounts/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="auth/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "accounts/reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="auth/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
     path(
         "password_change/",
@@ -52,21 +83,6 @@ urlpatterns = [
         "password_change/done/",
         auth_views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
-    ),
-    path(
-        "password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
     ),
     path("user/<pk>/", UserUpdateView.as_view(), name="user_edit"),
     path("requests/", ProviderRequestListView.as_view(), name="provider_request_list"),
