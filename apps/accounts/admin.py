77678aa46dc447798be846f3f018cfbd8562bc15
Oliwia Zaremba <git@olka.dev>
2023-06-13 14:46:31 +0200

    Only admins can manage object-level permissions

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.mail import send_mail
+from django.core.exceptions import PermissionDenied
 from django.conf import settings
 from django.contrib import messages
 from django.contrib import admin
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.admin import UserAdmin, GroupAdmin, Group
-import django.forms as dj_forms
 from django.utils.safestring import mark_safe
 from django.shortcuts import redirect, render
 from django.utils.translation import gettext_lazy as _
@@ -31,6 +31,7 @@ from taggit_labels.widgets import LabelWidget
 
 import logging
 import markdown
+import django.forms as dj_forms
 
 from dal_select2 import views as dal_select2_views
 
@@ -259,49 +260,94 @@ class LabelAutocompleteView(dal_select2_views.Select2QuerySetView):
 
 
 class ObjectPermissionsAdminMixin(GuardedModelAdminMixin):
-    managed_permissions = (manage_provider, manage_datacenter)
+    """
+    Define a custom mixin for object-level permissions to tweak the default GuardedModelAdminMixin.
+    """
+
+    # maintain a list of permissions that are managed via guardian admin
+    allowed_permissions = (manage_provider, manage_datacenter)
+
+    # define a custom template to only display the button to manage permissions to admin users
+    change_form_template = "admin/guardian/change_form.html"
+
+    def obj_perms_manage_view(self, request, object_pk):
+        """
+        Override a parent method to only allow admin users
+        to access the object permissions view (first screen)
+        """
+        if request.user.is_admin:
+            return super().obj_perms_manage_view(request, object_pk)
+        raise PermissionDenied
+
+    def obj_perms_manage_user_view(self, request, object_pk, user_id):
+        """
+        Override a parent method to only allow admin users
+        to access the user permissions management view (second screen)
+        """
+        if request.user.is_admin:
+            return super().obj_perms_manage_user_view(request, object_pk, user_id)
+        raise PermissionDenied
+
+    def obj_perms_manage_group_view(self, request, object_pk, group_id):
+        """
+        Override a parent method to only allow admin users
+        to access the group permissions management view (second screen)
+        """
+        if request.user.is_admin:
+            return super().obj_perms_manage_group_view(request, object_pk, group_id)
+        raise PermissionDenied
 
     def get_obj_perms_base_context(self, request, obj):
         """
-        Overwrites a method from GuardedModelAdminMixin to only display
-        the `manage_provider` permission in the permission management form.
+        Override a parent method in order to filter out permissions
+        on the object-level permissions view (first screen)
 
-        Django creates a default set of permissions for each object, for Hostinprovider:
+        Django creates a default set of permissions for each model,
+        for example for a Hostinprovider model:
         - add_hostingprovider,
         - change_hostingprovider,
         - delete_hostingprovider,
         - view_hostingprovider.
 
-        We only want to display the `manage_provider` permission.
+        We only want to filter out the list of permissions
+        to only display those defined in `allowed_permissions`
         """
         context = super().get_obj_perms_base_context(request, obj)
         model_perms = context.get("model_perms")
         managed_perms = model_perms.filter(
-            codename__in=[p.codename for p in self.managed_permissions]
+            codename__in=[p.codename for p in self.allowed_permissions]
         )
         context.update({"model_perms": managed_perms})
         return context
 
+    class ObjectLevelPermsForm(AdminUserObjectPermissionsForm):
+        """
+        Define a custom form for managing object-level permissions
+        """
+
+        def get_obj_perms_field_choices(self):
+            """
+            Override a parent method in order to filter out
+            the permission list displayed in the permission management form.
+            The permissions list will only include those defined in `allowed_permissions`.
+            """
+            perms = super().get_obj_perms_field_choices()
+            managed_perms = [
+                p.astuple() for p in ObjectPermissionsAdminMixin.allowed_permissions
+            ]
+            return [p for p in perms if p in managed_perms]
+
     def get_obj_perms_manage_user_form(self, request):
         """
-        Overwrites a method from GuardedModelAdminMixin to return a custom form.
-        This form is used for the second step of managing object-level permissions:
-        editing a given user.
+        Use custom form for managing user permissions
         """
+        return self.ObjectLevelPermsForm
 
-        class ObjectLevelPermsUserForm(AdminUserObjectPermissionsForm):
-            def get_obj_perms_field_choices(self):
-                """
-                Overwrites a method from the parent class to filter out the default Django permissions
-                for the Hostingprovider model and only return the `manage_provider` permission.
-                """
-                perms = super().get_obj_perms_field_choices()
-                managed_perms = [
-                    p.astuple() for p in ObjectPermissionsAdminMixin.managed_permissions
-                ]
-                return [p for p in perms if p in managed_perms]
-
-        return ObjectLevelPermsUserForm
+    def get_obj_perms_manage_group_form(self, request):
+        """
+        Use custom form to manage group permissions
+        """
+        return self.ObjectLevelPermsForm
 
 
 @admin.register(Hostingprovider, site=greenweb_admin)
