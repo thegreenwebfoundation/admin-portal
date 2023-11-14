0812fdf4cb40a44e330df5a984e0911d1d21512a
Chris Adams <chris@productscience.co.uk>
2023-11-14 15:57:08 +0100

    Add more convenient way to import our importers in code

--- apps/greencheck/importers/__init__.py
+++ apps/greencheck/importers/__init__.py
@@ -1,4 +1,5 @@
 from .importer_csv import CSVImporter
 from .importer_amazon import AmazonImporter
-
+from .importer_equinix import EquinixImporter
+from .importer_microsoft import MicrosoftImporter
 from .network_importer import NetworkImporter
