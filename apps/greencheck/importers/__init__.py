5c405fe1d3adaaecdd03c40c24ae49d49e5af34a
Chris Adams <chris@productscience.co.uk>
2024-07-01 16:25:43 +0200

    Add management command, tests and tidy up code

--- apps/greencheck/importers/__init__.py
+++ apps/greencheck/importers/__init__.py
@@ -2,4 +2,5 @@ from .importer_csv import CSVImporter  # noqa
 from .importer_amazon import AmazonImporter  # noqa
 from .importer_equinix import EquinixImporter  # noqa
 from .importer_microsoft import MicrosoftImporter  # noqa
+from .importer_cloudflare import CloudflareImporter  # noqa
 from .network_importer import NetworkImporter  # noqa
--- apps/greencheck/importers/importer_cloudflare.py
+++ apps/greencheck/importers/importer_cloudflare.py
@@ -12,10 +12,10 @@ logger = logging.getLogger(__name__)
 
 
 class CloudflareImporter:
-    def __init__(cls):
-        cls.hosting_provider_id = settings.CLOUDFLARE_PROVIDER_ID
+    def __init__(self):
+        self.hosting_provider_id = settings.CLOUDFLARE_PROVIDER_ID
 
-    def process(self, list_of_addresses):
+    def process(self, list_of_addresses: list[str]):
         provider = Hostingprovider.objects.get(id=settings.CLOUDFLARE_PROVIDER_ID)
 
         network_importer = NetworkImporter(provider)
@@ -23,30 +23,40 @@ class CloudflareImporter:
         network_importer.deactivate_asns()
         return network_importer.process_addresses(list_of_addresses)
 
-    def fetch_data_from_source(cls) -> list:
+    def fetch_data_from_source(self) -> list:
+        """
+        Fetch the contents of the two cloudflare endpoints, and return a list of 
+        IP networks ready to be processed.
+        """
         try:
             ipv4_response = requests.get(settings.CLOUDFLARE_REMOTE_API_ENDPOINT_IPV4)
             ipv6_response = requests.get(settings.CLOUDFLARE_REMOTE_API_ENDPOINT_IPV6)
             ipv4_data = ipv4_response.text
             ipv6_data = ipv6_response.text
-            return ipv4_data, ipv6_data
+
+            # destructure the lines of each text file, to make one longer list
+            return *ipv4_data.splitlines(), *ipv6_data.splitlines()
         except requests.RequestException:
             logger.warning("Unable to fetch text files. Aborting early.")
 
-    def parse_to_list(self, raw_data) -> List[Union[str, Tuple]]:
+    def parse_to_list(self, raw_data: list[str]) -> List[Union[str, Tuple]]:
+        """
+        Accept a list of IP networks listed in the remote text file and 
+        return a list of IP networks.
+        """
         try:
             list_of_ips = []
-            for line in raw_data.splitlines():
-                # Filter out the lines with network information
-                # (i.e. ip with subnet or AS numbers)
-                if line.startswith("AS") or line[0].isdigit():
-                    list_of_ips.append(
-                        line.split(" ", 1)[0]
-                    )  # Format as follows: IP range/ASN, Naming
+            for line in raw_data:
+                # Filter out empty lines
+                if not line:
+                    continue
 
+                # (i.e. ip with subnet
+                if line.startswith("AS") or line[0].isdigit():
+                    list_of_ips.append(line)
             return list_of_ips
         except Exception:
             logger.exception("Something really unexpected happened. Aborting")
 
 
-assert isinstance(CloudflareImporter:(), ImporterProtocol)
+assert isinstance(CloudflareImporter(), ImporterProtocol)
