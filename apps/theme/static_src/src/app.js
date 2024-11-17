7b214080ee1e605a2a0c7f226db1a5deff4dbdb5
Chris Adams <chris@productscience.co.uk>
2024-11-17 08:45:07 +0100

    Remove code for compiling unused charts

--- apps/theme/static_src/src/app.js
+++ apps/theme/static_src/src/app.js
@@ -1,5 +1,1 @@
-import statsCharts from './stats-charts'
-
-console.debug({ chartData: window.chartData})
-const ctx = document.getElementById("chart-last-30-days")
-const res = statsCharts.render(ctx, window.chartData)
+console.log("Compiled app.js")
