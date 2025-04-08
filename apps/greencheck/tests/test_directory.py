e9243223d66a174e80202f0f5b7e342b79b0d478
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-04-08 08:45:30 +0200

    Add carbon.txt badge in directory
    This commit adds a new carbon_txt_url field to hosting providers, which
    can be populated in the admin console, and a "carbon txt" badge to be
    shown on directory listings for those providers which have a carbon_txt
    url set. This allows us to highlight the presence of carbon.txt in the
    directory for providers that are using it, while side-stepping some of
    the issues pending resolution with domain verification for now.

--- apps/greencheck/tests/test_directory.py
+++ apps/greencheck/tests/test_directory.py
@@ -56,6 +56,46 @@ def test_templates_in_filter_view(client, hosting_provider_factory):
     templates = [tpl.name for tpl in res.templates]
     assert "greencheck/partials/_directory_results.html" in templates
 
+@pytest.mark.django_db
+def test_carbon_txt_template_included_for_provider_with_carbon_txt(client, hosting_provider_factory):
+    """
+    Check that we include the carbon_txt badge when a provider has a carbon_txt url
+    """
+
+    # given: a hosting provider in Germany
+    hosting_provider_factory.create(country="DE", showonwebsite=True, carbon_txt_url="https://www.example.com/carbon.txt")
+
+    # when: we visit our directory
+    res = client.get(reverse("directory-index"))
+
+    # then: we should get a successful response
+    assert res.status_code == 200
+
+    # and: we should see the "has results" template in our list of templates
+    # in use
+    templates = [tpl.name for tpl in res.templates]
+    assert "greencheck/partials/_directory_carbon_txt_badge.html" in templates
+
+@pytest.mark.django_db
+def test_carbon_txt_template_not_included_for_provider_without_carbon_txt(client, hosting_provider_factory):
+    """
+    Check that we do not include the carbon_txt badge when a provider has no carbon_txt url
+    """
+
+    # given: a hosting provider in Germany
+    hosting_provider_factory.create(country="DE", showonwebsite=True, carbon_txt_url=None)
+
+    # when: we visit our directory
+    res = client.get(reverse("directory-index"))
+
+    # then: we should get a successful response
+    assert res.status_code == 200
+
+    # and: we should see the "has results" template in our list of templates
+    # in use
+    templates = [tpl.name for tpl in res.templates]
+    assert "greencheck/partials/_directory_carbon_txt_badge.html" not in templates
+
 
 @pytest.mark.django_db
 def test_fallback_when_no_filter_view_has_no_results(client, hosting_provider_factory):
