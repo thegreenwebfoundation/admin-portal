e526765dd5862d99cfa4f86946bb90b05816cc6a
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-06-20 17:54:02 +0200

    Refactor accounts forms into separate files

---
+++ apps/accounts/forms/linked_domains.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from ..models import LinkedDomain
+
+class LinkedDomainFormStep0(forms.ModelForm):
+    """
+    First step of the Domain Linking process.
+    there is currently only one form as the second step just uses
+    the dummy PreviewForm with a custom template.
+    """
+
+    is_primary = forms.TypedChoiceField(
+        label="Is this your organisation's primary domain?",
+        # help_text=(
+        #     "Select this option if this domain represents your own hosting provider "
+        #     "itself, rather than a customer or some other entity."
+        # ),
+       coerce=lambda x: x == 'True',
+       choices=((True, "This is the organisation's primary domain."), (False, "This is a subdomain, a domain for another entity, or a customer's domain.")),
+       required=True,
+       widget=forms.RadioSelect
+    )
+
+    class Meta:
+        model = LinkedDomain
+        fields = ["domain", "is_primary"]
