492ff4462675c594c3a4252080ed137a45982e1d
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-10-15 09:48:33 +0200

    The clone_db script is meant to be used in staging, in order to produce a complete, safe, copy of the production DB and assets for testing purposes.
    The original motivation for producing this script was to avoid the
    possibility of data loss if URLs for production assets were copied over
    to staging in the db dump (the theory being that a deletion on staging
    would then delete the production asset), but this in fact isn't the case
    - the URLs / keys of assets are the same on staging / production, but
      they use different buckets, therefore, in the worst case, the asset
      wouldn't be available on staging, and production would be unaffected.
    
    It still seems useful to have a way of producing a complete
    copy of production on staging. However, I'm a little concerned that we've now *introduced* a
    vector for data loss, rather than eliminating one - running the clone_db
    script **in production** has the potential to wipe out production
    data if incorrectly configured.
    
    I've attempted to document the behaviour clearly and comprehensively,
    and include plenty of manual confirmation to prevent this from
    happening, but perhaps there's a conversation to be had about whether
    the benefits of having this script at all (uploaded files will be
    accessible in staging after restoring the db from a dump) outweigh the
    risks (running the script in production erroneously could lead to data
    loss).

---
+++ scripts/clone_db.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This script entirely resets the current database (given in DATABASE_URL)
+# and object storage bucket (given in OBJECT_STORAGE_BUCKET_NAME)
+# from another environment, given as a mysql dump file with CLONE_FROM_DB_DUMP
+# and the name of a source bucket, given in CLONE_FROM_OBJECT_STORAGE_BUCKET.
+
+set -euo pipefail
+
+if [[ -z $CLONE_FROM_DB_DUMP || -z $CLONE_FROM_OBJECT_STORAGE_BUCKET ]]; then
+    cat << EndOfMessage
+clone_db.sh requires the following environment variables to be set:
+   - CLONE_FROM_DB_DUMP: The path to the db dump file to import
+   - CLONE_FROM_OBJECT_STORAGE_BUCKET: The name of the object storage bucket to import assets from.
+In addition, it assumes the following environment variables, taken from your .env file:
+   - DATABASE_URL: The connection url for the database you want to import TO
+   - OBJECT_STORAGE_BUCKET_NAME: The name of the object storage bucket to clone assets TO
+
+It also requires mcli to be correctly configured to access both buckets, with the prefix "s3/"
+EndOfMessage
+    exit 2;
+fi
+
+DATABASE_USER=$(echo $DATABASE_URL | grep -oP "mysql://\K(.+?):" | cut -d: -f1)
+DATABASE_PASSWORD=$(echo $DATABASE_URL | grep -oP "mysql://.*:\K(.+?)@" | cut -d@ -f1)
+DATABASE_HOST=$(echo $DATABASE_URL | grep -oP "mysql://.*@\K(.+?):" | cut -d: -f1)
+DATABASE_PORT=$(echo $DATABASE_URL | grep -oP "mysql://.*@.*:\K(\d+)/" | cut -d/ -f1)
+DATABASE_NAME=$(echo $DATABASE_URL | grep -oP "mysql://.*@.*:.*/\K(.+?)$")
+
+cat << EndOfMessage
+WARNING! clone_db.sh is a DESTRUCTIVE operation!
+If you continue, the following will happen:
+- Your database at ${DATABASE_URL} will be cleared.
+- The database dump at ${CLONE_FROM_DB_DUMP} will be imported into ${DATABASE_URL}
+- All the files in the object storage bucket "${CLONE_FROM_OBJECT_STORAGE_BUCKET}" will be cloned into "${OBJECT_STORAGE_BUCKET_NAME}", overwriting any existing files with the same key.
+Are you sure you want to continue?
+EndOfMessage
+
+read  -n 1 -p "Confirm (y/N):" confirm
+
+if [ "$confirm" = "y" ]; then
+    echo "\nCloning database..."
+    mysql -u $DATABASE_USER -p$DATABASE_PASSWORD -h $DATABASE_HOST -P $DATABASE_PORT $DATABASE_NAME < $CLONE_FROM_DB_DUMP
+    echo "Cloning S3 bucket..."
+    mcli mirror s3/$CLONE_FROM_OBJECT_STORAGE_BUCKET s3/$OBJECT_STORAGE_BUCKET_NAME --overwrite
+    echo "Done!"
+else
+    echo "\nAborting!"
+    exit 1;
+fi
