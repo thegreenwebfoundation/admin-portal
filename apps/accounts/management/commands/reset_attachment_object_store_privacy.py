e680cdf81b5af0592986bfe00ad7d8bdaa68a1e3
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2026-03-03 19:17:14 +0100

    Add migration to set S3 bucket privacy

---
+++ apps/accounts/management/commands/reset_attachment_object_store_privacy.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from botocore.exceptions import ClientError
+from apps.accounts.models import HostingProviderSupportingDocument
+
+
+class Command(BaseCommand):
+    help = "Ensure that the S3 ACL privacy for all Supporting Documents relects their privacy settings in the Database"
+
+    def handle(self, *args, **options):
+        print("Resetting S3 ACL privacy for all documents with attachments")
+        documents = HostingProviderSupportingDocument.objects.filter(attachment__isnull=False).exclude(attachment__exact="").all()
+        for document in documents:
+            print(".", end="")
+            try:
+                document.set_object_store_privacy()
+                document.save()
+            except ClientError:
+                print(f"\n No document found at {document.attachment.name} for document with id {document.id}")
+                pass
+        print("\nDone.")
