9a11ddf37c337bc0c16e5ca99d82b7dabd80c959
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2026-03-05 14:50:41 +0100

    nicer output in management command

--- apps/accounts/management/commands/reset_attachment_object_store_privacy.py
+++ apps/accounts/management/commands/reset_attachment_object_store_privacy.py
@@ -7,14 +7,14 @@ class Command(BaseCommand):
     help = "Ensure that the S3 ACL privacy for all Supporting Documents relects their privacy settings in the Database"
 
     def handle(self, *args, **options):
-        print("Resetting S3 ACL privacy for all documents with attachments")
+        self.stdout.write("Resetting S3 ACL privacy for all documents with attachments")
         documents = HostingProviderSupportingDocument.objects_all.filter(attachment__isnull=False).exclude(attachment__exact="").all()
         for document in documents:
-            print(".", end="")
             try:
                 document.set_object_store_privacy()
                 document.save()
+                self.stdout.write(".", ending="")
             except ClientError:
-                print(f"\n No document found at {document.attachment.name} for document with id {document.id}")
+                self.stderr.write(self.style.ERROR(f"\n No document found at {document.attachment.name} for document with id {document.id}"))
                 pass
-        print("\nDone.")
+        self.stdout.write(self.style.SUCCESS("\nDone."))
