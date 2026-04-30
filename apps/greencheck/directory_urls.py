cecaa58d783ea410299bcdb44d2f147192935110
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2026-04-30 07:59:22 +0200

    Speed up directory - visible providers by country index, page caching and http response conmpression

--- apps/greencheck/directory_urls.py
+++ apps/greencheck/directory_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 from .views import DirectoryView
 
 urlpatterns = [
-    path("", DirectoryView.as_view(), name="directory-index"),
+    path("", cache_page(settings.DIRECTORY_CACHE_TIMEOUT)(DirectoryView.as_view()), name="directory-index"),
 ]
