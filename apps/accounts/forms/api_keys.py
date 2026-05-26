47fead654f6f77a0ccfdc939a8c93fefd4188a46
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2026-05-26 17:50:01 +0200

    privacy statement acceptance

--- apps/accounts/forms/api_keys.py
+++ apps/accounts/forms/api_keys.py
@@ -1,7 +1,12 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 from ..models import APIKey
 
+class FieldsetRadioSelect(forms.RadioSelect):
+    def render(self, name, value, attrs=None, renderer=None):
+        html = super().render(name, value, attrs, renderer)
+        return mark_safe(f'<fieldset class="mt-2">{html}</fieldset>')
 
 class APIAccessForm(forms.Form):
     api_access_motivation = forms.CharField(
@@ -9,6 +14,20 @@ class APIAccessForm(forms.Form):
         label="What is your intended use case?",
         help_text="It's very useful to us to understand how and why people are using our data and services. Please tell us a little about how you plan to use our APIs."
     )
+
+    privacy_policy_acceptance = forms.ChoiceField(
+        required=False,
+        choices=[("Yes", "Yes"), ("No", "No")],
+        widget=FieldsetRadioSelect(),
+        label=mark_safe("Do you accept our <a href='https://www.thegreenwebfoundation.org/privacy-statement/' target='_blank'>privacy statement</a>?"),
+        help_text="We log user activity with authenticated APIs, to better understand how our service is used, and to monitor usage levels - we need your explicit informed consent for this."
+    )
+
+    def clean_privacy_policy_acceptance(self):
+        acceptance = self.cleaned_data.get("privacy_policy_acceptance")
+        if acceptance != "Yes":
+            raise forms.ValidationError("You must agree to the privacy policy in order to use the API.")
+        return acceptance
 
     def update_user(self, user):
         user.api_access_motivation = self.cleaned_data["api_access_motivation"]
